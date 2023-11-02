fb77326ba281cdef81a96e85f8ab1d05e9416bb7
Eric Ringold <ericringold@gmail.com>
2023-11-02 13:37:13 -0600

    update test to use latest 10k run

--- postprocessing/test/test_comstock_to_eia_comparison.py
+++ postprocessing/test/test_comstock_to_eia_comparison.py
@@ -13,8 +13,8 @@ def test_plot_generation():
     # ComStock
     comstock = comstockpostproc.comstock.ComStock(
         s3_base_dir='eulp/euss_com',
-        comstock_run_name='cycle2_euss_full_350k_long_2of2_1555',
-        comstock_run_version='cycle2_euss_full_350k_long_2of2_1555',
+        comstock_run_name='baseline_data_10k',
+        comstock_run_version='baseline_data_10k',
         comstock_year=2018,
         truth_data_version='v01',
         buildstock_csv_name='buildstock.csv',
@@ -22,7 +22,7 @@ def test_plot_generation():
         drop_failed_runs=True,
         color_hex='#0072B2',
         skip_missing_columns=True,
-        athena_table_name='cycle2_euss_full_350k_long_2of2_1555',
+        athena_table_name='baseline_data_10k',
         reload_from_csv=False,
         include_upgrades=False
         )
@@ -32,16 +32,19 @@ def test_plot_generation():
         cbecs_year = 2018,
         truth_data_version='v01',
         color_hex='#009E73',
-        reload_from_csv=False, # True since CSV already made and want faster reload times
+        reload_from_csv=True, # True since CSV already made and want faster reload times
         )
 
     # EIA
     eia = comstockpostproc.eia.EIA(
         year=2018,
         truth_data_version="v01",
-        reload_from_csv=False
+        reload_from_csv=True
     )
 
+    # save cbecs csv
+    cbecs.export_to_csv_wide()
+    
     # Scale ComStock run to CBECS 2018 AND remove non-ComStock buildings from CBECS
     comstock.add_national_scaling_weights(cbecs, remove_non_comstock_bldg_types_from_cbecs=True)
     comstock.export_to_csv_wide()
@@ -49,8 +52,10 @@ def test_plot_generation():
     # Make a comparison by passing in a list of EIA and ComStock runs to compare
     eia_list = [eia]
     comstock_list = [comstock]
-    comp = comstockpostproc.comstock_to_eia_comparison.ComStockToEIAComparison(eia_list, comstock_list)
+    comp = comstockpostproc.comstock_to_eia_comparison.ComStockToEIAComparison(eia_list, comstock_list, make_comparison_plots=True)
     comp.monthly_data
+    comp.export_to_csv_wide()
 
     # # Generate comparison plots
     # comp.plot_monthly_energy_consumption_for_eia()
+    
