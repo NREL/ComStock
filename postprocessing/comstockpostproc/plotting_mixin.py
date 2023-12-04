7ae4293e602f7acbbc5a42024582cab83f827869
Eric Ringold <ericringold@gmail.com>
2023-12-04 14:20:15 -0700

    resolve RuntimeWarning more than 20 figures opened

--- postprocessing/comstockpostproc/plotting_mixin.py
+++ postprocessing/comstockpostproc/plotting_mixin.py
@@ -1406,6 +1406,7 @@ class PlottingMixin():
                 fig_name = f'com_eia_{title.replace(" ", "_").lower()}.{self.image_type}'
                 fig_path = os.path.join(output_dir, fig_name)
                 plt.savefig(fig_path, bbox_inches = 'tight')
+                plt.close()
 
     def plot_monthly_energy_consumption_for_eia(self, df, color_map, output_dir):
         # Columns to summarize
@@ -1468,3 +1469,4 @@ class PlottingMixin():
                     fig_name = f'com_eia_{title.replace(" ", "_").lower()}.{self.image_type}'
                     fig_path = os.path.join(output_dir, fig_name) 
                     plt.savefig(fig_path, bbox_inches = 'tight')
+                    plt.close()
