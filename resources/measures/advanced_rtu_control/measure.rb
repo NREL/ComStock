cd826515759396cab58a0e337dfbb0a604a205c6
Allen <aallen@nrel.gov>
2023-12-06 12:57:37 -0700

    adding template for advanced RTU measure

---
+++ resources/measures/advanced_rtu_control/measure.rb
@@ -0,0 +1,73 @@
+# insert your copyright here
+
+# see the URL below for information on how to write OpenStudio measures
+# http://nrel.github.io/OpenStudio-user-documentation/reference/measure_writing_guide/
+
+# start the measure
+class AdvancedRTUControl < OpenStudio::Measure::ModelMeasure
+  # human readable name
+  def name
+    # Measure name should be the title case of the class name.
+    return 'AdvancedRTUControl'
+  end
+
+  # human readable description
+  def description
+    return 'Replace this text with an explanation of what the measure does in terms that can be understood by a general building professional audience (building owners, architects, engineers, contractors, etc.).  This description will be used to create reports aimed at convincing the owner and/or design team to implement the measure in the actual building design.  For this reason, the description may include details about how the measure would be implemented, along with explanations of qualitative benefits associated with the measure.  It is good practice to include citations in the measure if the description is taken from a known source or if specific benefits are listed.'
+  end
+
+  # human readable description of modeling approach
+  def modeler_description
+    return 'Replace this text with an explanation for the energy modeler specifically.  It should explain how the measure is modeled, including any requirements about how the baseline model must be set up, major assumptions, citations of references to applicable modeling resources, etc.  The energy modeler should be able to read this description and understand what changes the measure is making to the model and why these changes are being made.  Because the Modeler Description is written for an expert audience, using common abbreviations for brevity is good practice.'
+  end
+
+  # define the arguments that the user will input
+  def arguments(model)
+    args = OpenStudio::Measure::OSArgumentVector.new
+
+    # the name of the space to add to the model
+    space_name = OpenStudio::Measure::OSArgument.makeStringArgument('space_name', true)
+    space_name.setDisplayName('New space name')
+    space_name.setDescription('This name will be used as the name of the new space.')
+    args << space_name
+
+    return args
+  end
+
+  # define what happens when the measure is run
+  def run(model, runner, user_arguments)
+    super(model, runner, user_arguments)  # Do **NOT** remove this line
+
+    # use the built-in error checking
+    if !runner.validateUserArguments(arguments(model), user_arguments)
+      return false
+    end
+
+    # assign the user inputs to variables
+    space_name = runner.getStringArgumentValue('space_name', user_arguments)
+
+    # check the space_name for reasonableness
+    if space_name.empty?
+      runner.registerError('Empty space name was entered.')
+      return false
+    end
+
+    # report initial condition of model
+    runner.registerInitialCondition("The building started with #{model.getSpaces.size} spaces.")
+
+    # add a new space to the model
+    new_space = OpenStudio::Model::Space.new(model)
+    new_space.setName(space_name)
+
+    # echo the new space's name back to the user
+    runner.registerInfo("Space #{new_space.name} was added.")
+
+    # report final condition of model
+    runner.registerFinalCondition("The building finished with #{model.getSpaces.size} spaces.")
+
+    return true
+  end
+end
+
+# register the measure to be used by the application
+AdvancedRTUControl.new.registerWithApplication
