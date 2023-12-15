27ee452665d33734447177e1494ce1aac143990d
Eric Ringold <ericringold@gmail.com>
2023-12-15 16:19:50 -0700

    support plotting baseline and upgrades

--- postprocessing/comstockpostproc/comstock_to_eia_comparison.py
+++ postprocessing/comstockpostproc/comstock_to_eia_comparison.py
@@ -5,6 +5,7 @@ import os
 import logging
 
 import pandas as pd
+import polars as pl
 from typing import List
 
 from comstockpostproc.naming_mixin import NamingMixin
@@ -16,7 +17,7 @@ from comstockpostproc.comstock import ComStock
 logger = logging.getLogger(__name__)
 
 class ComStockToEIAComparison(NamingMixin, UnitsMixin, PlottingMixin):
-    def __init__(self, comstock_list: List[ComStock], eia_list: List[EIA], image_type='jpg', name=None, make_comparison_plots=True):
+    def __init__(self, comstock_list: List[ComStock], eia_list: List[EIA], upgrade_id, image_type='jpg', name=None, make_comparison_plots=True):
 
         # Initialize members
         self.comstock_list = comstock_list
@@ -35,18 +36,35 @@ class ComStockToEIAComparison(NamingMixin, UnitsMixin, PlottingMixin):
                 logger.warning(f'No monthly_data was available for {dataset.dataset_name}, not including in EIA comparison.')
                 continue
             if isinstance(dataset, ComStock):
-                dfs_to_concat.append(dataset.monthly_data.to_pandas())
+                if upgrade_id == 'All':
+                    monthly_data = dataset.monthly_data.to_pandas()
+                    monthly_data[dataset.DATASET] = monthly_data[dataset.DATASET] + ' - ' + monthly_data['upgrade_name']
+                    dfs_to_concat.append(monthly_data)
+                    up_name_map = dict(zip(monthly_data['upgrade'].unique(), monthly_data['upgrade_name'].unique()))
+                    for upgrade_id in monthly_data['upgrade'].unique():
+                        dataset_name = dataset.dataset_name + ' - ' + up_name_map[upgrade_id]
+                        dataset_names.append(dataset_name)
+                        self.color_map[dataset_name] = dataset.color
+                elif upgrade_id not in dataset.monthly_data['upgrade']:
+                    logger.error(f'Upgrade {upgrade_id} not found in {dataset.dataset_name}. Enter a valid upgrade ID in the ComStockToEIAComparison constructor or \"All\" to include all upgrades.')
+                else:
+                    monthly_data = dataset.monthly_data.filter(pl.col('upgrade') == upgrade_id).to_pandas()
+                    monthly_data[dataset.DATASET] = monthly_data[dataset.DATASET] + ' - ' + monthly_data['upgrade_name']
+                    dfs_to_concat.append(monthly_data)
+                    dataset_name = dataset.dataset_name + ' - ' + monthly_data.iloc[0]['upgrade_name']
+                    self.color_map[dataset_name] = dataset.color
+                    dataset_names.append(dataset_name)
             else:
                 dfs_to_concat.append(dataset.monthly_data)
-            self.color_map[dataset.dataset_name] = dataset.color
-            dataset_names.append(dataset.dataset_name)
+                self.color_map[dataset.dataset_name] = dataset.color
+                dataset_names.append(dataset.dataset_name)
 
         # Name the comparison
         if self.name is None:
-            self.name = ' vs '.join(dataset_names)
+            self.name = ' vs '.join(sorted(dataset_names,key=len)[:2])
 
         # Combine into a single dataframe for convenience
-        self.monthly_data = pd.concat(dfs_to_concat, join='inner', ignore_index=True)
+        self.monthly_data = pd.concat(dfs_to_concat, join='outer', ignore_index=True)
         current_dir = os.path.dirname(os.path.abspath(__file__))
 
         # Make directories
