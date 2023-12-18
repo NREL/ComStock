f5418e4a064a3a39f12c62b896b7ac2b570a7644
Eric Ringold <ericringold@gmail.com>
2023-12-18 09:41:44 -0700

    add color gradient helpers

--- postprocessing/comstockpostproc/plotting_mixin.py
+++ postprocessing/comstockpostproc/plotting_mixin.py
@@ -1470,3 +1470,48 @@ class PlottingMixin():
                     fig_path = os.path.join(output_dir, fig_name) 
                     plt.savefig(fig_path, bbox_inches = 'tight')
                     plt.close()
+
+    # color functions from https://bsouthga.dev/posts/color-gradients-with-python
+    def linear_gradient(self, start_hex, finish_hex="#FFFFFF", n=10):
+        ''' returns a gradient list of (n) colors between
+            two hex colors. start_hex and finish_hex
+            should be the full six-digit color string,
+            inlcuding the number sign ("#FFFFFF") '''
+        # Starting and ending colors in RGB for
+        s = self.hex_to_RGB(start_hex)
+        f = self.hex_to_RGB(finish_hex)
+        # Initilize a list of the output colors with the starting color
+        RGB_list = [s]
+        # Calcuate a color at each evenly spaced value of t from 1 to n
+        for t in range(1, n):
+            # Interpolate RGB vector for color at the current value of t
+            curr_vector = [
+            int(s[j] + (float(t)/(n-1))*(f[j]-s[j]))
+            for j in range(3)
+            ]
+            # Add it to our list of output colors
+            RGB_list.append(curr_vector)
+
+        return self.color_dict(RGB_list)
+    
+    def color_dict(self, gradient):
+        ''' Takes in a list of RGB sub-lists and returns dictionary of
+            colors in RGB and hex form for use in a graphing function
+            defined later on '''
+        return {"hex":[self.RGB_to_hex(RGB) for RGB in gradient],
+            "r":[RGB[0] for RGB in gradient],
+            "g":[RGB[1] for RGB in gradient],
+            "b":[RGB[2] for RGB in gradient]}
+        
+    def hex_to_RGB(self, hex):
+        ''' "#FFFFFF" -> [255,255,255] '''
+        # Pass 16 to the integer function for change of base
+        return [int(hex[i:i+2], 16) for i in range(1,6,2)]
+    
+    def RGB_to_hex(self, RGB):
+        ''' [255,255,255] -> "#FFFFFF" '''
+        # Components need to be integers for hex to make sense
+        RGB = [int(x) for x in RGB]
+        return "#"+"".join(["0{0:x}".format(v) if v < 16 else
+                    "{0:x}".format(v) for v in RGB])
+  
