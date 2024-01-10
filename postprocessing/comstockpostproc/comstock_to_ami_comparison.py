1618967c98c89a02006ca2c8690cedcf1b0fa09e
Matthew Dahlhausen <matthew.dahlhausen@gmail.com>
2024-01-10 14:01:32 -0700

    plot if there are >=3 bldgs in ami instead of 5

--- postprocessing/comstockpostproc/comstock_to_ami_comparison.py
+++ postprocessing/comstockpostproc/comstock_to_ami_comparison.py
@@ -110,12 +110,12 @@ class ComStockToAMIComparison(NamingMixin, UnitsMixin, PlottingMixin):
                 if comstock_check.empty:
                     logger.debug(f"dataset does not contain {building_type} buildings in {comstock_data_label} for region {region['source_name']}. Skipping building specific graphics.")
                     continue
-                
+
                 # check that ami data has enough buildings to provide a useful comparison
                 ami_min_count = int(ami_check['bldg_count'].min())
-                if ami_min_count < 5:
-                    print(f"dataset contains fewer than 5 {building_type} buildings in {ami_data_label} for region {region['source_name']}. Skipping building specific graphics.")
-                    logger.debug(f"dataset contains fewer than 5 {building_type} buildings in {ami_data_label} for region {region['source_name']}. Skipping building specific graphics.")
+                if ami_min_count < 3:
+                    print(f"dataset contains fewer than 3 {building_type} buildings in {ami_data_label} for region {region['source_name']}. Skipping building specific graphics.")
+                    logger.debug(f"dataset contains fewer than 3 {building_type} buildings in {ami_data_label} for region {region['source_name']}. Skipping building specific graphics.")
                     continue
 
                 type_region_plot_data = self.plot_day_type_comparison_stacked_by_enduse(type_region_df, region, building_type, self.color_map, bldg_type_output_dir)
@@ -123,6 +123,6 @@ class ComStockToAMIComparison(NamingMixin, UnitsMixin, PlottingMixin):
                 self.plot_day_type_comparison_stacked_by_enduse(type_region_df, region, building_type, self.color_map, bldg_type_output_dir, normalization='Daytype')
                 self.plot_day_type_comparison_stacked_by_enduse(type_region_df, region, building_type, self.color_map, bldg_type_output_dir, normalization='Annual')
                 self.plot_load_duration_curve(type_region_df, region, building_type, self.color_map, bldg_type_output_dir)
-        
+
         # combine plot data
         self.ami_plot_data = pd.concat(dfs_to_concat, join='outer', ignore_index=True)
