e84375d6a9e3b672dfce35f1ce152712c11fa36d
Amy Van Sant <41447070+amylebar@users.noreply.github.com>
2024-02-23 13:31:44 -0700

    add parentheses around or statements

--- resources/measures/upgrade_hvac_add_heat_pump_rtu/measure.rb
+++ resources/measures/upgrade_hvac_add_heat_pump_rtu/measure.rb
@@ -780,15 +780,15 @@ class AddHeatPumpRtu < OpenStudio::Measure::ModelMeasure
     h_eir_allstages_ff = nil
     if std_perf
       # if cchpc scenarios are set, use those curves. else, use the standard performance curves
-      if cchpc=='scenario_1' || cchpc=='scenario_4'
+      if (cchpc=='scenario_1') || (cchpc=='scenario_4')
         # read performance data
         path_data_curve = "#{File.dirname(__FILE__)}/resources/performance_map_CCHP_v1.json"
         custom_data_json = JSON.parse(File.read(path_data_curve))
-      elsif cchpc=='scenario_2' || cchpc=='scenario_5'
+      elsif (cchpc=='scenario_2') || (cchpc=='scenario_5')
         # read performance data
         path_data_curve = "#{File.dirname(__FILE__)}/resources/performance_map_CCHP_v2.json"
         custom_data_json = JSON.parse(File.read(path_data_curve))
-      elsif cchpc=='scenario_3' || cchpc=='scenario_6'
+      elsif (cchpc=='scenario_3') || (cchpc=='scenario_6')
         # read performance data
         path_data_curve = "#{File.dirname(__FILE__)}/resources/performance_map_CCHP_v3.json"
         custom_data_json = JSON.parse(File.read(path_data_curve))
@@ -1582,9 +1582,9 @@ class AddHeatPumpRtu < OpenStudio::Measure::ModelMeasure
       new_dx_cooling_coil_speed1.setGrossRatedSensibleHeatRatio(0.872821200315651)
       # if cchpc scenarios are set, use fixed cop. else, use the default cop.
       if cchpc != 'false'
-        if cchpc == 'scenario_1' || cchpc == 'scenario_2' || cchpc == 'scenario_3'
+        if (cchpc == 'scenario_1') || (cchpc == 'scenario_)2' || (cchpc == 'scenario_3')
           rated_cop_fit = 3.60
-        elsif cchpc == 'scenario_4' || cchpc == 'scenario_5' || cchpc == 'scenario_6'
+        elsif (cchpc == 'scenario_4') || (cchpc == 'scenario_5') || (cchpc == 'scenario_6')
           rated_cop_fit = 4.00
         end
         new_dx_cooling_coil_speed1.setGrossRatedCoolingCOP(rated_cop_fit)
@@ -1617,9 +1617,9 @@ class AddHeatPumpRtu < OpenStudio::Measure::ModelMeasure
       new_dx_cooling_coil_speed2.setGrossRatedSensibleHeatRatio(0.80463149283227)
       # if cchpc scenarios are set, use fixed cop. else, use the default cop
       if cchpc != 'false'
-        if cchpc == 'scenario_1' || cchpc == 'scenario_2' || cchpc == 'scenario_3'
+        if (cchpc == 'scenario_1') || (cchpc == 'scenario_2') || (cchpc == 'scenario_3')
           rated_cop_fit = 3.60
-        elsif cchpc == 'scenario_4' || cchpc == 'scenario_5' || cchpc == 'scenario_6'
+        elsif (cchpc == 'scenario_4') || (cchpc == 'scenario_5') || (cchpc == 'scenario_6')
           rated_cop_fit = 4.00
         end
         new_dx_cooling_coil_speed2.setGrossRatedCoolingCOP(rated_cop_fit)
@@ -1652,9 +1652,9 @@ class AddHeatPumpRtu < OpenStudio::Measure::ModelMeasure
       new_dx_cooling_coil_speed3.setGrossRatedSensibleHeatRatio(0.79452681573034)
       # if cchpc scenarios are set, use fixed cop. else, use the standard performance regression method
       if std_perf
-        if cchpc == 'scenario_1' || cchpc == 'scenario_2' || cchpc == 'scenario_3'
+        if (cchpc == 'scenario_1') || (cchpc == 'scenario_2') || (cchpc == 'scenario_3')
           rated_cop_fit = 3.60
-        elsif cchpc == 'scenario_4' || cchpc == 'scenario_5' || cchpc == 'scenario_6'
+        elsif (cchpc == 'scenario_4') || (cchpc == 'scenario_5') || (cchpc == 'scenario_6')
           rated_cop_fit = 4.00
         else
           rated_cop_fit = get_rated_cop_cooling(air_loop_hvac, hash_clg_airflow_stgs[3], hash_clg_cap_stgs[3])
@@ -1697,9 +1697,9 @@ class AddHeatPumpRtu < OpenStudio::Measure::ModelMeasure
       new_dx_cooling_coil_speed4.setGrossRatedSensibleHeatRatio(0.784532541812955)
       # if cchpc scenarios are set, use fixed cop. else, use the standard performance regression method
       if std_perf
-        if cchpc == 'scenario_1' || cchpc == 'scenario_2' || cchpc == 'scenario_3'
+        if (cchpc == 'scenario_1') || (cchpc == 'scenario_2') || (cchpc == 'scenario_3')
           rated_cop_fit = 3.60
-        elsif cchpc == 'scenario_4' || cchpc == 'scenario_5' || cchpc == 'scenario_6'
+        elsif (cchpc == 'scenario_4') || (cchpc == 'scenario_5') || (cchpc == 'scenario_6')
             rated_cop_fit = 4.00
         else
           rated_cop_fit = get_rated_cop_cooling(air_loop_hvac, hash_clg_airflow_stgs[4], hash_clg_cap_stgs[4])
@@ -1914,11 +1914,11 @@ class AddHeatPumpRtu < OpenStudio::Measure::ModelMeasure
       if std_perf
         new_dx_heating_coil = OpenStudio::Model::CoilHeatingDXSingleSpeed.new(model)
         new_dx_heating_coil.setName("#{air_loop_hvac.name} Heat Pump Coil")
-        if cchpc == 'scenario_1' || cchpc == 'scenario_4'
+        if (cchpc == 'scenario_1') || (cchpc == 'scenario_4')
           compress_lockout_temp = OpenStudio.convert(-15, 'F', 'C').get
-        elsif cchpc == 'scenario_2' || cchpc == 'scenario_5'
+        elsif (cchpc == 'scenario_2)' || (cchpc == 'scenario_5')
           compress_lockout_temp = OpenStudio.convert(-13, 'F', 'C').get
-        elsif cchpc == 'scenario_3' || cchpc == 'scenario_6'
+        elsif (cchpc == 'scenario_3') || (cchpc == 'scenario_6')
           compress_lockout_temp = OpenStudio.convert(5, 'F', 'C').get
         else
           compress_lockout_temp = -17.7778
@@ -1932,11 +1932,11 @@ class AddHeatPumpRtu < OpenStudio::Measure::ModelMeasure
         new_dx_heating_coil.setDefrostTimePeriodFraction(0.058333)
 
         new_dx_heating_coil.setRatedTotalHeatingCapacity(hash_htg_cap_stgs[4])
-        if cchpc == 'scenario_1' || cchpc == 'scenario_4'
+        if (cchpc == 'scenario_1') || (cchpc == 'scenario_4')
           rated_cop_fit = 4.35
-        elsif cchpc == 'scenario_2' || cchpc == 'scenario_5'
+        elsif (cchpc == 'scenario_2') || (cchpc == 'scenario_5')
           rated_cop_fit = 4.17
-        elsif cchpc == 'scenario_3' || cchpc == 'scenario_6'
+        elsif (cchpc == 'scenario_3') || (cchpc == 'scenario_6')
           rated_cop_fit = 3.85
         else
           rated_cop_fit = get_rated_cop_heating(air_loop_hvac, hash_htg_airflow_stgs[4], hash_htg_cap_stgs[4])  
