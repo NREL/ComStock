24bcd935dc07e849c6a92c21d8499670c3d571f5
Andrew Parker <Andrew.Parker@nrel.gov>
2024-05-13 21:03:57 -0600

    Updates old Singularity commands to Apptainer equivalent

--- build/singularity/build_singularity.sh
+++ build/singularity/build_singularity.sh
@@ -2,5 +2,5 @@
 
 # Build singularity container
 cd singularity
-SINGULARITY_NOHTTPS=1 singularity build docker-openstudio.simg docker-daemon://127.0.0.1:5000/docker-openstudio:latest
+APPTAINER_NOHTTPS=1 apptainer build docker-openstudio.simg docker-daemon://127.0.0.1:5000/docker-openstudio:latest
 mv docker-openstudio.simg ..
