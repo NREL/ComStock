0b8abd97eef3e6c8cf36b06d354fc5e936cb5707
Taylor <ltaylor2@nrel.gov>
2024-07-17 08:01:39 -0600

    Update compare_comstock_to_cbecs.py

--- postprocessing/compare_comstock_to_cbecs.py
+++ postprocessing/compare_comstock_to_cbecs.py
@@ -12,8 +12,13 @@ def main():
     # ComStock run
     comstock = cspp.ComStock(
         s3_base_dir='eulp/euss_com',  # If run not on S3, download results_up**.parquet manually
+<<<<<<< Updated upstream
         comstock_run_name='lower_sat_stpt_10k_v2',  # Name of the run on S3
         comstock_run_version='lower_sat_stpt_10k_v2',  # Use whatever you want to see in plot and folder names
+=======
+        comstock_run_name='lower_sat_stpt_10k',  # Name of the run on S3
+        comstock_run_version='lower_sat_stpt_10k',  # Use whatever you want to see in plot and folder names
+>>>>>>> Stashed changes
         comstock_year=2018,  # Typically don't change this
         athena_table_name= None,  # Typically same as comstock_run_name or None
         truth_data_version='v01',  # Typically don't change this
@@ -23,7 +28,7 @@ def main():
         color_hex='#0072B2',  # Color used to represent this run in plots
         skip_missing_columns=True,  # False if you want to ensure you have all data specified for exportb
         reload_from_csv=False,  # True if CSV already made and want faster reload times
-        include_upgrades=True,  # False if not looking at upgrades
+        include_upgrades=False,  # False if not looking at upgrades
         upgrade_ids_to_skip=[]  # Use [1, 3] etc. to exclude certain upgrades
     )
 
@@ -34,14 +39,32 @@ def main():
         color_hex='#009E73',  # Color used to represent CBECS in plots
         reload_from_csv=False  # True if CSV already made and want faster reload times
         )
-    cbecs.export_to_csv_wide()
+
+    # From Feburary 2024 EUSS Full Run
+    national_scaling_factors = {
+                'Hospital': 7.40279973438571,
+                'SecondarySchool': 7.61402397003761,
+                'Outpatient': 4.08817512805358,
+                'LargeHotel': 9.52500448561723,
+                'FullServiceRestaurant': 7.56685751697572,
+                'RetailStripmall': 1.87065215852945,
+                'RetailStandalone': 3.20458438519356,
+                'PrimarySchool': 9.72943522508755,
+                'QuickServiceRestaurant': 8.53126424238864,
+                'SmallOffice': 8.72605822017302,
+                'SmallHotel': 3.3063363735822,
+                'MediumOffice': 9.32502232347711,
+                'LargeOffice': 5.75996046109096,
+                'Warehouse': 3.23792487389949
+            }
+
     # Scale ComStock run to CBECS 2018 AND remove non-ComStock buildings from CBECS
-    comstock.add_national_scaling_weights(cbecs, remove_non_comstock_bldg_types_from_cbecs=True)
+    comstock.add_national_scaling_weights(cbecs, remove_non_comstock_bldg_types_from_cbecs=True, use_hard_coded_scaling_weights=False, scaling_factors=national_scaling_factors)
     comstock.export_to_csv_wide()
 
     # Make a comparison by passing in a list of CBECs and ComStock runs to compare
     # upgrade_id can be 'All' or the upgrade number
-    comp = cspp.ComStockToCBECSComparison(cbecs_list=[cbecs], comstock_list=[comstock], upgrade_id='All',make_comparison_plots=True)
+    comp = cspp.ComStockToCBECSComparison(cbecs_list=[cbecs], comstock_list=[comstock], upgrade_id='All',make_comparison_plots=True, make_hvac_plots=True)
 
     comp.export_to_csv_wide()
 
