84d7d841c74c9ec2165ade0426007bb90112dbc1
Landan Taylor <landan.taylor@nrel.gov>
2024-09-24 16:20:05 -0600

    Update light_led_test.rb

--- resources/measures/upgrade_light_led/tests/light_led_test.rb
+++ resources/measures/upgrade_light_led/tests/light_led_test.rb
@@ -1,17 +1,61 @@
 # ComStock™, Copyright (c) 2023 Alliance for Sustainable Energy, LLC. All rights reserved.
 # See top level LICENSE.txt file for license terms.
+
+# *******************************************************************************
+# OpenStudio(R), Copyright (c) 2008-2018, Alliance for Sustainable Energy, LLC.
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# (1) Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# (2) Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# (3) Neither the name of the copyright holder nor the names of any contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission from the respective party.
+#
+# (4) Other than as required in clauses (1) and (2), distributions in any form
+# of modifications or other derivative works may not use the "OpenStudio"
+# trademark, "OS", "os", or any other confusingly similar designation without
+# specific prior written permission from Alliance for Sustainable Energy, LLC.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) AND ANY CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S), ANY CONTRIBUTORS, THE
+# UNITED STATES GOVERNMENT, OR THE UNITED STATES DEPARTMENT OF ENERGY, NOR ANY OF
+# THEIR EMPLOYEES, BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# *******************************************************************************
+
+# dependencies
 require 'openstudio'
-require 'openstudio/ruleset/ShowRunnerOutput'
+require 'openstudio/measure/ShowRunnerOutput'
+require 'fileutils'
 require 'minitest/autorun'
 require_relative '../measure.rb'
-require 'fileutils'
-require_relative '../../../../test/helpers/minitest_helper'
 
+# only necessary to include here if annual simulation request and the measure doesn't require openstudio-standards
+require 'openstudio-standards'
 
-class LightLightingTechnologyTest < MiniTest::Test
+class LightLED_Test < Minitest::Test
+  # all tests are a sub definition of this class, e.g.:
+  # def test_new_kind_of_test
+  #   # test content
+  # end
 
   def test_number_of_arguments_and_argument_names
     # this test ensures that the current test is matched to the measure inputs
+    test_name = 'test_number_of_arguments_and_argument_names'
+    puts "\n######\nTEST:#{test_name}\n######\n"
 
     # create an instance of the measure
     measure = LightLED.new
@@ -25,281 +69,175 @@ class LightLightingTechnologyTest < MiniTest::Test
     assert_equal('lighting_generation', arguments[0].name)
   end
 
-  def test_outpatient_no_change
-    # create an instance of the measure
-    measure = LightLED.new
-
-    # create runner with empty OSW
-    osw = OpenStudio::WorkflowJSON.new
-    runner = OpenStudio::Measure::OSRunner.new(osw)
-
-    # load the test model
-    translator = OpenStudio::OSVersion::VersionTranslator.new
-    path = "#{File.dirname(__FILE__)}/outpatient.osm"
-    model = translator.loadModel(path)
-    assert(!model.empty?)
-    model = model.get
-
-    # get arguments
-    arguments = measure.arguments(model)
-    argument_map = OpenStudio::Measure::OSArgumentMap.new
-
-    lighting_generation = arguments[0].clone
-    assert(lighting_generation.setValue('gen6_led'))
-    argument_map['lighting_generation'] = lighting_generation
-
-    # run the measure
-    measure.run(model, runner, argument_map)
-    result = runner.result
-
-    # show the output
-    show_output(result)
-
-    # assert that it ran correctly
-    assert_equal('NA', result.value.valueName)
-
-    # save the model to test output directory
-    output_file_path = "#{File.dirname(__FILE__)}/output/test_outpatient.osm"
-    model.save(output_file_path, true)
+  # return file paths to test models in test directory
+  def models_for_tests
+    paths = Dir.glob(File.join(File.dirname(__FILE__), '../../../tests/models/*.osm'))
+    paths = paths.map { |path| File.expand_path(path) }
+    return paths
   end
 
-  def test_hospital
-    # create an instance of the measure
-    measure = LightLED.new
-
-    # create runner with empty OSW
-    osw = OpenStudio::WorkflowJSON.new
-    runner = OpenStudio::Measure::OSRunner.new(osw)
-
-    # load the test model
-    translator = OpenStudio::OSVersion::VersionTranslator.new
-    path = "#{File.dirname(__FILE__)}/hospital.osm"
-    model = translator.loadModel(path)
-    assert(!model.empty?)
-    model = model.get
-
-    # get arguments
-    arguments = measure.arguments(model)
-    argument_map = OpenStudio::Measure::OSArgumentMap.new
-
-    lighting_generation = arguments[0].clone
-    assert(lighting_generation.setValue('gen5_led'))
-    argument_map['lighting_generation'] = lighting_generation
-
-    # run the measure
-    measure.run(model, runner, argument_map)
-    result = runner.result
-
-    # show the output
-    show_output(result)
-
-    # assert that it ran correctly
-    assert_equal('Success', result.value.valueName)
-
-    # save the model to test output directory
-    output_file_path = "#{File.dirname(__FILE__)}/output/test_hospital.osm"
-    model.save(output_file_path, true)
+  # return file paths to epw files in test directory
+  def epws_for_tests
+    paths = Dir.glob(File.join(File.dirname(__FILE__), '../../../tests/weather/*.epw'))
+    paths = paths.map { |path| File.expand_path(path) }
+    return paths
   end
 
-  def test_secondary_school
-    # create an instance of the measure
-    measure = LightLED.new
-
-    # create runner with empty OSW
-    osw = OpenStudio::WorkflowJSON.new
-    runner = OpenStudio::Measure::OSRunner.new(osw)
-
-    # load the test model
+  def load_model(osm_path)
     translator = OpenStudio::OSVersion::VersionTranslator.new
-    path = "#{File.dirname(__FILE__)}/secondary_school_gen1.osm"
-    model = translator.loadModel(path)
+    model = translator.loadModel(OpenStudio::Path.new(osm_path))
     assert(!model.empty?)
     model = model.get
+    return model
+  end
 
-    # get arguments
-    arguments = measure.arguments(model)
-    argument_map = OpenStudio::Measure::OSArgumentMap.new
+  def run_dir(test_name)
+    # always generate test output in specially named 'output' directory so result files are not made part of the measure
+    return "#{File.dirname(__FILE__)}/output/#{test_name}"
+  end
 
-    lighting_generation = arguments[0].clone
-    assert(lighting_generation.setValue('gen5_led'))
-    argument_map['lighting_generation'] = lighting_generation
+  def model_output_path(test_name)
+    return "#{run_dir(test_name)}/#{test_name}.osm"
+  end
 
-    # run the measure
-    measure.run(model, runner, argument_map)
-    result = runner.result
+  def sql_path(test_name)
+    return "#{run_dir(test_name)}/run/eplusout.sql"
+  end
 
-    # show the output
-    show_output(result)
+  def report_path(test_name)
+    return "#{run_dir(test_name)}/reports/eplustbl.html"
+  end
 
-    # assert that it ran correctly
-    assert_equal('Success', result.value.valueName)
+  # applies the measure and then runs the model
+  def apply_measure_and_run(test_name, measure, argument_map, osm_path, epw_path, run_model: false)
+    assert(File.exist?(osm_path))
+    assert(File.exist?(epw_path))
 
-    # make sure it assigned a high bay fixture to the gym spaces
-    model.getSpaceTypes.each do |space_type|
-      next unless space_type.name.to_s == 'SecondarySchool Gym'
-      space_type.lights.each do |light|
-        next unless light.name.to_s == 'SecondarySchool Gym General Lighting'
-        light_definition = light.lightsDefinition
-        lighting_technology = light_definition.additionalProperties.getFeatureAsString('lighting_technology').to_s
-        assert_equal(lighting_technology, 'LED high bay luminaire')
-      end
+    # create run directory if it does not exist
+    if !File.exist?(run_dir(test_name))
+      FileUtils.mkdir_p(run_dir(test_name))
+    end
+    assert(File.exist?(run_dir(test_name)))
+
+    # change into run directory for tests
+    start_dir = Dir.pwd
+    Dir.chdir run_dir(test_name)
+
+    # remove prior runs if they exist
+    if File.exist?(model_output_path(test_name))
+      FileUtils.rm(model_output_path(test_name))
+    end
+    if File.exist?(report_path(test_name))
+      FileUtils.rm(report_path(test_name))
     end
 
-    # save the model to test output directory
-    output_file_path = "#{File.dirname(__FILE__)}/output/test_secondary_school.osm"
-    model.save(output_file_path, true)
-  end
-
-  def test_small_office
-    # create an instance of the measure
-    measure = LightLED.new
-
-    # create runner with empty OSW
-    osw = OpenStudio::WorkflowJSON.new
-    runner = OpenStudio::Measure::OSRunner.new(osw)
+    # copy the osm and epw to the test directory
+    new_osm_path = "#{run_dir(test_name)}/#{File.basename(osm_path)}"
+    FileUtils.cp(osm_path, new_osm_path)
+    new_epw_path = "#{run_dir(test_name)}/#{File.basename(epw_path)}"
+    FileUtils.cp(epw_path, new_epw_path)
+    # create an instance of a runner
+    runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
 
     # load the test model
-    translator = OpenStudio::OSVersion::VersionTranslator.new
-    path = "#{File.dirname(__FILE__)}/small_office.osm"
-    model = translator.loadModel(path)
-    assert(!model.empty?)
-    model = model.get
+    model = load_model(new_osm_path)
 
-    # get arguments
-    arguments = measure.arguments(model)
-    argument_map = OpenStudio::Measure::OSArgumentMap.new
-
-    lighting_generation = arguments[0].clone
-    assert(lighting_generation.setValue('gen5_led'))
-    argument_map['lighting_generation'] = lighting_generation
+    # set model weather file
+    epw_file = OpenStudio::EpwFile.new(OpenStudio::Path.new(new_epw_path))
+    OpenStudio::Model::WeatherFile.setWeatherFile(model, epw_file)
+    assert(model.weatherFile.is_initialized)
 
     # run the measure
+    puts "\nAPPLYING MEASURE..."
     measure.run(model, runner, argument_map)
     result = runner.result
+    result_success = result.value.valueName == 'Success'
 
     # show the output
     show_output(result)
 
-    # assert that it ran correctly
-    assert_equal('Success', result.value.valueName)
+    # save model
+    model.save(model_output_path(test_name), true)
 
-    # save the model to test output directory
-    output_file_path = "#{File.dirname(__FILE__)}/output/test_small_office.osm"
-    model.save(output_file_path, true)
+    if run_model && result_success
+      puts "\nRUNNING MODEL..."
+
+      std = Standard.build('ComStock DEER 2020')
+      std.model_run_simulation_and_log_errors(model, run_dir(test_name))
+
+      # check that the model ran successfully
+      assert(File.exist?(sql_path(test_name)))
+    end
+
+    # change back directory
+    Dir.chdir(start_dir)
+
+    return result
   end
 
-  def test_strip_mall_no_change
-    # create an instance of the measure
-    measure = LightLED.new
-
-    # create runner with empty OSW
-    osw = OpenStudio::WorkflowJSON.new
-    runner = OpenStudio::Measure::OSRunner.new(osw)
-
-    # load the test model
-    translator = OpenStudio::OSVersion::VersionTranslator.new
-    path = "#{File.dirname(__FILE__)}/strip_mall_gen4.osm"
-    model = translator.loadModel(path)
-    assert(!model.empty?)
-    model = model.get
-
-    # get arguments
-    arguments = measure.arguments(model)
-    argument_map = OpenStudio::Measure::OSArgumentMap.new
-
-    lighting_generation = arguments[0].clone
-    assert(lighting_generation.setValue('gen5_led'))
-    argument_map['lighting_generation'] = lighting_generation
-
-    # run the measure
-    measure.run(model, runner, argument_map)
-    result = runner.result
-
-    # show the output
-    show_output(result)
-
-    # assert that it ran correctly
-    assert_equal('NA', result.value.valueName)
-
-    # save the model to test output directory
-    output_file_path = "#{File.dirname(__FILE__)}/output/test_small_office_no_change.osm"
-    model.save(output_file_path, true)
+  # create an array of hashes with model name, weather, and expected result
+  def models_to_test
+    test_sets = []
+    test_sets << { model: 'Outpatient_VAV_economizer_test', weather: 'CA_LOS-ANGELES-DOWNTOWN-USC_722874S_16', result: 'Success', arg_hash: { 'lighting_generation' => 'gen6_led' } }
+    test_sets << { model: 'SecondarySchool_Pre1980_5A', weather: 'CA_LOS-ANGELES-DOWNTOWN-USC_722874S_16', result: 'NA', arg_hash: { 'lighting_generation' => 'gen5_led' } }
+    test_sets << { model: 'Small_Office_2A', weather: 'CA_LOS-ANGELES-DOWNTOWN-USC_722874S_16', result: 'Success', arg_hash: { 'lighting_generation' => 'gen5_led' } }
+    return test_sets
   end
 
-    def test_retailstandalone_no_change
-    # create an instance of the measure
-    measure = LightLED.new
+  def test_models
+    test_name = 'test_models'
+    puts "\n######\nTEST:#{test_name}\n######\n"
 
-    # create runner with empty OSW
-    osw = OpenStudio::WorkflowJSON.new
-    runner = OpenStudio::Measure::OSRunner.new(osw)
+    models_to_test.each do |set|
+      instance_test_name = set[:model]
+      puts "instance test name: #{instance_test_name}"
+      osm_path = models_for_tests.select { |x| set[:model] == File.basename(x, '.osm') }
+      epw_path = epws_for_tests.select { |x| set[:weather] == File.basename(x, '.epw') }
+      assert(!osm_path.empty?)
+      assert(!epw_path.empty?)
+      osm_path = osm_path[0]
+      epw_path = epw_path[0]
 
-    # load the test model
-    translator = OpenStudio::OSVersion::VersionTranslator.new
-    path = "#{File.dirname(__FILE__)}/RetailStandalone.osm"
-    model = translator.loadModel(path)
-    assert(!model.empty?)
-    model = model.get
+      # create an instance of the measure
+      measure = LightLED.new
 
-    # get arguments
-    arguments = measure.arguments(model)
-    argument_map = OpenStudio::Measure::OSArgumentMap.new
+      # load the model; only used here for populating arguments
+      model = load_model(osm_path)
 
-    lighting_generation = arguments[0].clone
-    assert(lighting_generation.setValue('gen7_led'))
-    argument_map['lighting_generation'] = lighting_generation
+      # set arguments here; will vary by measure
+      arguments = measure.arguments(model)
+      argument_map = OpenStudio::Measure::OSArgumentMap.new
 
-    # run the measure
-    measure.run(model, runner, argument_map)
-    result = runner.result
+      # set default arguments
+      arguments.each do |arg|
+        temp_arg_var = arg.clone
+        argument_map[arg.name] = temp_arg_var # Add argument to map with default value
+      end
 
-    # show the output
-    show_output(result)
+      # override with values from arg_hash
+      args_hash = set[:arg_hash]
+      args_hash.each do |arg_name, arg_value|
+        arg = arguments.find { |a| a.name == arg_name }
+        raise "Argument #{arg_name} not found" if arg.nil?
+        assert(arg.setValue(arg_value)) # Override with value from arg_hash
+        argument_map[arg_name] = arg
+      end
 
-    # assert that it ran correctly
-    assert_equal('NA', result.value.valueName)
+      # apply the measure to the model and optionally run the model
+      result = apply_measure_and_run(instance_test_name, measure, argument_map, osm_path, epw_path, run_model: false)
 
-    # save the model to test output directory
-    output_file_path = "#{File.dirname(__FILE__)}/output/test_retailstandalone.osm"
-    model.save(output_file_path, true)
-  end
+      # check the measure result; result values will equal Success, Fail, or Not Applicable
+      # also check the amount of warnings, info, and error messages
+      # use if or case statements to change expected assertion depending on model characteristics
+      assert(result.value.valueName == set[:result], "Expected result: #{set[:result]}, got: #{result.value.valueName} in #{instance_test_name}")
 
-  def test_RtL
-    # create an instance of the measure
-    measure = LightLED.new
+      # to check that something changed in the model, load the model and the check the objects match expected new value
+      model = load_model(model_output_path(instance_test_name))
 
-    # create runner with empty OSW
-    osw = OpenStudio::WorkflowJSON.new
-    runner = OpenStudio::Measure::OSRunner.new(osw)
+      # add additional tests here to check model outputs
 
-    # load the test model
-    translator = OpenStudio::OSVersion::VersionTranslator.new
-    path = "#{File.dirname(__FILE__)}/RtL.osm"
-    model = translator.loadModel(path)
-    assert(!model.empty?)
-    model = model.get
 
-    # get arguments
-    arguments = measure.arguments(model)
-    argument_map = OpenStudio::Measure::OSArgumentMap.new
-
-    lighting_generation = arguments[0].clone
-    assert(lighting_generation.setValue('gen7_led'))
-    argument_map['lighting_generation'] = lighting_generation
-
-    # run the measure
-    measure.run(model, runner, argument_map)
-    result = runner.result
-
-    # show the output
-    show_output(result)
-
-    # assert that it ran correctly
-    assert_equal('Success', result.value.valueName)
-
-    # save the model to test output directory
-    output_file_path = "#{File.dirname(__FILE__)}/output/test_RtL.osm"
-    model.save(output_file_path, true)
+    end
   end
 
 end
