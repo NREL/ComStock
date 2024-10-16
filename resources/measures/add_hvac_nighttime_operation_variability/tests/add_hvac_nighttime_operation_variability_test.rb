90f803cdebf456b63e7266085af2ce06434f58c2
Matthew Dahlhausen <matthew.dahlhausen@gmail.com>
2024-10-16 09:53:58 -0600

    update add_hvac_nighttime_operation_variability

--- resources/measures/add_hvac_nighttime_operation_variability/tests/add_hvac_nighttime_operation_variability_test.rb
+++ resources/measures/add_hvac_nighttime_operation_variability/tests/add_hvac_nighttime_operation_variability_test.rb
@@ -1,22 +1,118 @@
 # ComStock™, Copyright (c) 2023 Alliance for Sustainable Energy, LLC. All rights reserved.
 # See top level LICENSE.txt file for license terms.
 
-
+# dependencies
+require 'fileutils'
+require 'minitest/autorun'
 require 'openstudio'
 require 'openstudio/measure/ShowRunnerOutput'
-require 'minitest/autorun'
-require_relative '../measure.rb'
-require_relative '../../../../test/helpers/minitest_helper'
-require 'fileutils'
+require_relative '../measure'
 
 class AddHvacNighttimeOperationVariabilityTest < Minitest::Test
-  # def setup
-  # end
+  # return file paths to test models in test directory
+  def models_for_tests
+    paths = Dir.glob(File.join(__dir__, '../../../tests/models/*.osm'))
+    paths = paths.map { |path| File.expand_path(path) }
+    return paths
+  end
 
-  # def teardown
-  # end
+  # return file paths to epw files in test directory
+  def epws_for_tests
+    paths = Dir.glob(File.join(__dir__, '../../../tests/weather/*.epw'))
+    paths = paths.map { |path| File.expand_path(path) }
+    return paths
+  end
+
+  def load_model(osm_path)
+    translator = OpenStudio::OSVersion::VersionTranslator.new
+    model = translator.loadModel(OpenStudio::Path.new(osm_path))
+    assert(!model.empty?)
+    model = model.get
+    return model
+  end
+
+  def run_dir(test_name)
+    # always generate test output in specially named 'output' directory so result files are not made part of the measure
+    return "#{__dir__}/output/#{test_name}"
+  end
+
+  def model_output_path(test_name)
+    return "#{run_dir(test_name)}/out.osm"
+  end
+
+  def sql_path(test_name)
+    return "#{run_dir(test_name)}/run/eplusout.sql"
+  end
+
+  def report_path(test_name)
+    return "#{run_dir(test_name)}/reports/eplustbl.html"
+  end
+
+  # applies the measure and then runs the model
+  def apply_measure_and_run(test_name, measure, argument_map, osm_path, epw_path, run_model: false)
+    assert(File.exist?(osm_path))
+    assert(File.exist?(epw_path))
+
+    # remove prior runs if they exist
+    FileUtils.rm_f(model_output_path(test_name))
+    FileUtils.rm_f(sql_path(test_name))
+    FileUtils.rm_f(report_path(test_name))
+
+    # create run directory if it does not exist
+    FileUtils.mkdir_p(run_dir(test_name))
+
+    # create an instance of a runner with OSW
+    runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
+
+    # load the test model
+    model = load_model(osm_path)
+
+    # set model weather file
+    epw_file = OpenStudio::EpwFile.new(OpenStudio::Path.new(epw_path))
+    OpenStudio::Model::WeatherFile.setWeatherFile(model, epw_file)
+    assert(model.weatherFile.is_initialized)
+
+    # temporarily change directory to the run directory and run the measure
+    # only necessary for measures that do a sizing run
+    start_dir = Dir.pwd
+    begin
+      Dir.chdir(run_dir(test_name))
+
+      # run the measure
+      puts "\nAPPLYING MEASURE..."
+      reset_log
+      measure.run(model, runner, argument_map)
+      result = runner.result
+
+      # log messages
+      log_messages_to_file("#{run_dir(test_name)}/measure.log", false)
+    ensure
+      Dir.chdir(start_dir)
+    end
+
+    # show the output
+    show_output(result)
+
+    # save model
+    model.save(model_output_path(test_name), true)
+
+    if run_model && (result.value.valueName == 'Success')
+      puts "\nRUNNING MODEL..."
+
+      std = Standard.build('ComStock DEER 2020')
+      std.model_run_simulation_and_log_errors(model, run_dir(test_name))
+
+      # check that the model ran successfully
+      assert(File.exist?(sql_path(test_name)))
+    end
+
+    return result
+  end
 
   def test_number_of_arguments_and_argument_names
+    # this test ensures that the current test is matched to the measure inputs
+    puts "\n######\nTEST:#{__method__}\n######\n"
+
     # create an instance of the measure
     measure = AddHvacNighttimeOperationVariability.new
 
@@ -26,102 +122,57 @@ class AddHvacNighttimeOperationVariabilityTest < Minitest::Test
     # get arguments and test that they are what we are expecting
     arguments = measure.arguments(model)
     assert_equal(1, arguments.size)
-    assert_equal('space_name', arguments[0].name)
+    assert_equal('rtu_night_mode', arguments[0].name)
   end
 
-  def test_bad_argument_values
-    # create an instance of the measure
-    measure = AddHvacNighttimeOperationVariability.new
+  def test_models
+    puts "\n######\nTEST:#{__method__}\n######\n"
 
-    # create runner with empty OSW
-    osw = OpenStudio::WorkflowJSON.new
-    runner = OpenStudio::Measure::OSRunner.new(osw)
+    # create an array of hashes with model name, weather, expected result, and any specific
+    models_to_test = []
+    models_to_test << { model: 'LargeOffice_VAV_chiller_boiler', weather: 'CA_LOS-ANGELES-DOWNTOWN-USC_722874S_16', result: 'Success' }
 
-    # make an empty model
-    model = OpenStudio::Model::Model.new
+    models_to_test.each do |set|
+      instance_test_name = set[:model]
+      puts "instance test name: #{instance_test_name}"
+      osm_path = models_for_tests.select { |x| set[:model] == File.basename(x, '.osm') }
+      epw_path = epws_for_tests.select { |x| set[:weather] == File.basename(x, '.epw') }
+      assert(!osm_path.empty?)
+      assert(!epw_path.empty?)
+      osm_path = osm_path[0]
+      epw_path = epw_path[0]
 
-    # get arguments
-    arguments = measure.arguments(model)
-    argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
+      # create an instance of the measure
+      measure = AddHvacNighttimeOperationVariability.new
 
-    # create hash of argument values
-    args_hash = {}
-    args_hash['space_name'] = ''
+      # load the model; only used here for populating arguments
+      model = load_model(osm_path)
 
-    # populate argument with specified hash value if specified
-    arguments.each do |arg|
-      temp_arg_var = arg.clone
-      if args_hash.key?(arg.name)
-        assert(temp_arg_var.setValue(args_hash[arg.name]))
-      end
-      argument_map[arg.name] = temp_arg_var
+      # set arguments here; will vary by measure
+      arguments = measure.arguments(model)
+      argument_map = OpenStudio::Measure::OSArgumentMap.new
+
+      rtu_night_mode = arguments[0].clone
+      assert(rtu_night_mode.setValue('night_fancycle_novent'))
+      argument_map['rtu_night_mode'] = rtu_night_mode
+
+      # apply the measure to the model and optionally run the model
+      result = apply_measure_and_run(instance_test_name, measure, argument_map, osm_path, epw_path, run_model: false)
+
+      # check the measure result; result values will equal Success, Fail, or Not Applicable
+      # also check the amount of warnings, info, and error messages
+      # use if or case statements to change expected assertion depending on model characteristics
+      assert(result.value.valueName == set[:result])
+
+      # to check that something changed in the model, load the model and the check the objects match expected new value
+      model = load_model(model_output_path(instance_test_name))
+
+      # Check the schedules
+      air_loop_hvac = model.getAirLoopHVACs.first
+      assert(air_loop_hvac.airLoopHVACOutdoorAirSystem.is_initialized)
+      oa_system = air_loop_hvac.airLoopHVACOutdoorAirSystem.get.getControllerOutdoorAir
+      assert(oa_system.minimumOutdoorAirSchedule.is_initialized)
+      assert_equal("#{air_loop_hvac.name}_night_ventcycle_schedule", oa_system.minimumOutdoorAirSchedule.get.name.get)
     end
-
-    # run the measure
-    measure.run(model, runner, argument_map)
-    result = runner.result
-
-    # show the output
-    show_output(result)
-
-    # assert that it ran correctly
-    assert_equal('Fail', result.value.valueName)
-  end
-
-  def test_good_argument_values
-    # create an instance of the measure
-    measure = AddHvacNighttimeOperationVariability.new
-
-    # create runner with empty OSW
-    osw = OpenStudio::WorkflowJSON.new
-    runner = OpenStudio::Measure::OSRunner.new(osw)
-
-    # load the test model
-    translator = OpenStudio::OSVersion::VersionTranslator.new
-    path = "#{File.dirname(__FILE__)}/example_model.osm"
-    model = translator.loadModel(path)
-    assert(!model.empty?)
-    model = model.get
-
-    # store the number of spaces in the seed model
-    num_spaces_seed = model.getSpaces.size
-
-    # get arguments
-    arguments = measure.arguments(model)
-    argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
-
-    # create hash of argument values.
-    # If the argument has a default that you want to use, you don't need it in the hash
-    args_hash = {}
-    args_hash['space_name'] = 'New Space'
-    # using defaults values from measure.rb for other arguments
-
-    # populate argument with specified hash value if specified
-    arguments.each do |arg|
-      temp_arg_var = arg.clone
-      if args_hash.key?(arg.name)
-        assert(temp_arg_var.setValue(args_hash[arg.name]))
-      end
-      argument_map[arg.name] = temp_arg_var
-    end
-
-    # run the measure
-    measure.run(model, runner, argument_map)
-    result = runner.result
-
-    # show the output
-    show_output(result)
-
-    # assert that it ran correctly
-    assert_equal('Success', result.value.valueName)
-    assert(result.info.size == 1)
-    assert(result.warnings.empty?)
-
-    # check that there is now 1 space
-    assert_equal(1, model.getSpaces.size - num_spaces_seed)
-
-    # save the model to test output directory
-    output_file_path = "#{File.dirname(__FILE__)}//output/test_output.osm"
-    model.save(output_file_path, true)
   end
 end
