73aaf358755ab11e1b22e615fd2720929904c8fa
Matthew Dahlhausen <matthew.dahlhausen@gmail.com>
2024-10-17 21:29:44 -0600

    update test_hvac in create_typical

--- resources/measures/create_typical_building_from_model/tests/test_hvac.rb
+++ resources/measures/create_typical_building_from_model/tests/test_hvac.rb
@@ -6,6 +6,7 @@ require 'fileutils'
 require 'minitest/autorun'
 require 'openstudio'
 require 'openstudio/measure/ShowRunnerOutput'
+require 'openstudio-standards'
 require_relative '../measure'
 
 class AddHVACSystemTest < Minitest::Test
@@ -17,62 +18,61 @@ class AddHVACSystemTest < Minitest::Test
     # List all the HVAC system types to test
     # [system_type, allowable_htg_unmet_hrs, allowable_clg_unmet_hrs]
     hvac_systems = [
-      ['PTAC with hot water heat', 600, 600],
-      ['PTAC with gas coil heat', 600, 600],
-      ['PTAC with electric baseboard heat', 600, 600],
-      ['PTAC with no heat', 8760, 600],
-      ['PTAC with district hot water heat', 600, 600],
-      ['PTHP', 600, 600],
-      ['PSZ-AC with gas coil heat', 600, 600],
-      ['PSZ-AC with electric baseboard heat', 600, 600],
-      ['PSZ-AC with no heat', 8760, 600],
-      ['PSZ-AC with district hot water heat', 600, 600],
-      ['PSZ-HP', 600, 600],
-      ['Fan coil district chilled water with no heat', 8760, 600],
-      ['Fan coil district chilled water and boiler', 600, 600],
-      ['Fan coil district chilled water unit heaters', 600, 600],
-      ['Fan coil district chilled water electric baseboard heat', 600, 600],
-      ['Fan coil district hot and chilled water', 600, 600],
-      ['Fan coil district hot water and chiller', 600, 600],
-      ['Fan coil chiller with no heat', 8760, 600],
-      ['Baseboard district hot water heat', 600, 8760],
-      ['Baseboard district hot water heat with direct evap coolers', 600, 600],
+      ['Baseboard district hot water', 600, 8760],
       ['Baseboard electric heat', 600, 8760],
-      ['Baseboard electric heat with direct evap coolers', 600, 600],
-      ['Baseboard hot water heat', 600, 8760],
-      ['Baseboard hot water heat with direct evap coolers', 600, 600],
-      ['Window AC with no heat', 8760, 600],
-      ['Window AC with forced air furnace', 600, 600],
-      ['Window AC with district hot water baseboard heat', 600, 600],
-      ['Window AC with hot water baseboard heat', 600, 600],
-      ['Window AC with electric baseboard heat', 600, 600],
-      ['Window AC with unit heaters', 600, 600],
-      ['Direct evap coolers', 8760, 600],
-      ['Direct evap coolers with unit heaters', 600, 600],
-      ['Unit heaters', 600, 8760],
-      ['Heat pump heat with no cooling', 600, 8760],
-      ['Heat pump heat with direct evap cooler', 600, 600],
-      ['VAV with reheat', 600, 600],
-      ['VAV with PFP boxes', 600, 600],
-      ['VAV with gas reheat', 600, 600],
-      ['VAV with zone unit heaters', 600, 600],
-      ['VAV with electric baseboard heat', 600, 600],
-      ['VAV cool with zone heat pump heat', 600, 600], # Double airloop failure
-      ['PVAV with reheat', 600, 600],
-      ['PVAV with PFP boxes', 600, 600],
-      ['Residential forced air', 600, 8760],
-      ['Residential forced air cooling hot water baseboard heat', 600, 600],
-      ['Residential forced air with district hot water', 600, 600],
-      ['Residential heat pump', 600, 600],
+      ['Baseboard gas boiler', 600, 8760],
+      ['Direct evap coolers with baseboard district hot water', 600, 600],
+      ['Direct evap coolers with baseboard electric', 600, 600],
+      ['Direct evap coolers with baseboard gas boiler', 600, 600],
+      ['Direct evap coolers with no heat', 8760, 600],
+      ['Direct evap coolers with gas unit heaters', 600, 600],
+      ['Fan coil chiller with boiler', 600, 600],
+      ['Fan coil chiller with central air source heat pump', 600, 600],
+      ['Fan coil chiller with district hot water', 600, 600],
+      ['Fan coil chiller with baseboard electric', 600, 600],
+      ['Fan coil chiller with gas unit heaters', 600, 600],
+      ['Fan coil chiller with no heat', 8760, 600],
+      ['Fan coil district chilled water with district hot water', 600, 600],
+      ['Fan coil district chilled water with baseboard electric', 600, 600],
+      ['Fan coil district chilled water with gas unit heaters', 600, 600],
+      ['Fan coil district chilled water with no heat', 8760, 600],
       ['Forced air furnace', 600, 8760],
-      ['Forced air furnace district chilled water fan coil', 600, 600],
-      ['Forced air furnace direct evap cooler', 600, 600], # Double airloop failure
+      ['Gas unit heaters', 600, 8760],
+      ['PTAC with gas boiler', 600, 600],
+      ['PTAC with gas coil', 600, 600],
+      ['PTAC with baseboard electric', 600, 600],
+      ['PTAC with no heat', 8760, 600],
+      ['PTAC with district hot water', 600, 600],
+      ['PTHP', 600, 600],
+      ['PSZ-AC with baseboard electric', 600, 600],
+      ['PSZ-AC with gas coil', 600, 600],
+      ['PSZ-AC with district hot water', 600, 600],
+      ['PSZ-AC with no heat', 8760, 600],
+      ['PSZ-HP', 600, 600],
+      ['PVAV with gas boiler reheat', 600, 600],
+      ['PVAV with PFP boxes', 600, 600],
+      ['Residential AC with baseboard electric', 600, 600],
+      ['Residential AC with baseboard gas boiler', 600, 600],
       ['Residential AC with no heat', 8760, 600],
-      ['Residential AC with electric baseboard heat', 600, 600]
+      ['Residential heat pump', 600, 600],
+      ['Residential heat pump with no cooling', 600, 8760],
+      ['Residential forced air furnace', 600, 600],
+      ['VAV chiller with gas boiler reheat', 600, 600],
+      ['VAV chiller with central air source heat pump reheat', 600, 600],
+      ['VAV chiller with district hot water reheat', 600, 600],
+      ['VAV chiller with PFP boxes', 600, 600],
+      ['VAV chiller with gas coil reheat', 600, 600],
+      ['Window AC with baseboard electric', 600, 600],
+      ['Window AC with baseboard gas boiler', 600, 600],
+      ['Window AC with baseboard central air source heat pump', 600, 600],
+      ['Window AC with baseboard district hot water', 600, 600],
+      ['Window AC with forced air furnace', 600, 600],
+      ['Window AC with unit heaters', 600, 600],
+      ['Window AC with no heat', 8760, 600]
     ]
     template = '90.1-2013'
     building_type = 'SmallOffice'
-    climate_zone = 'ASHRAE 169-2006-2A'
+    climate_zone = 'ASHRAE 169-2013-2A'
 
     # Get the original working directory
     start_dir = Dir.pwd
@@ -90,22 +90,26 @@ class AddHVACSystemTest < Minitest::Test
         Dir.chdir(model_dir)
 
         # Load the model if already created
-        if File.exist?("#{model_dir}/final.osm")
+        if File.exist?("#{model_dir}/AR/run/eplusout.sql")
           puts "Already ran #{system_type}"
           model = OpenStudio::Model::Model.new
-          sql = safe_load_sql("#{model_dir}/AR/run/eplusout.sql")
+          sql = OpenStudio::SqlFile.new("#{model_dir}/AR/run/eplusout.sql")
           model.setSqlFile(sql)
         else # make and run annual simulation
           puts "Running #{system_type}"
 
           # Load the test model
-          model = safe_load_model("#{File.dirname(__FILE__)}/SmallOffice.osm")
+          translator = OpenStudio::OSVersion::VersionTranslator.new
+          model = translator.loadModel(OpenStudio::Path.new("#{__dir__}/SmallOffice.osm"))
+          assert(!model.empty?)
+          model = model.get
 
-          # Assign a weather file
-          model.add_design_days_and_weather_file(building_type, template, climate_zone, '')
+          # set the weather file for the test model
+          epw_file_path = "#{__dir__}/USA_TX_Houston-Bush.Intercontinental.AP.722430_TMY3.epw"
+          OpenstudioStandards::Weather.model_set_building_location(model,
+                                                                   weather_file_path: epw_file_path)
 
-          # Modify the unmet hours tolerance to a reasonable value
-          # of 1F
+          # Modify the unmet hours tolerance to a reasonable value of 1F
           unmet_hrs_tol_r = 1
           unmet_hrs_tol_k = OpenStudio.convert(unmet_hrs_tol_r, 'R', 'K').get
           tol = model.getOutputControlReportingTolerances
@@ -157,7 +161,8 @@ class AddHVACSystemTest < Minitest::Test
           model.save("#{model_dir}/final.osm", true)
 
           # Run the annual simulation
-          annual_run_success = model.run_simulation_and_log_errors("#{model_dir}/AR")
+          std = Standard.build(template)
+          annual_run_success = std.model_run_simulation_and_log_errors(model, "#{model_dir}/AR")
 
           # Log the errors
           log_messages_to_file("#{model_dir}/openstudio-standards.log", debug = false)
@@ -167,10 +172,10 @@ class AddHVACSystemTest < Minitest::Test
         end
 
         # Check the conditioned floor area
-        errs << "For #{system_type} there was no conditioned area." if model.net_conditioned_floor_area == 0
+        errs << "For #{system_type} there was no conditioned area." if model.getBuilding.conditionedFloorArea.get.zero?
 
         # Check the unmet heating hours
-        unmet_htg_hrs = model.annual_occupied_unmet_heating_hours
+        unmet_htg_hrs = OpenstudioStandards::SqlFile.model_get_annual_occupied_unmet_heating_hours(model)
         if unmet_htg_hrs
           errs << "For #{system_type} there were #{unmet_htg_hrs} unmet occupied heating hours, more than the limit of #{allowable_htg_unmet_hrs}." if unmet_htg_hrs > allowable_htg_unmet_hrs
         else
@@ -178,7 +183,7 @@ class AddHVACSystemTest < Minitest::Test
         end
 
         # Check the unmet cooling hours
-        unmet_clg_hrs = model.annual_occupied_unmet_cooling_hours
+        unmet_clg_hrs = OpenstudioStandards::SqlFile.model_get_annual_occupied_unmet_cooling_hours(model)
         if unmet_clg_hrs
           errs << "For #{system_type} there were #{unmet_clg_hrs} unmet occupied cooling hours, more than the limit of #{allowable_clg_unmet_hrs}." if unmet_clg_hrs > allowable_clg_unmet_hrs
         else
