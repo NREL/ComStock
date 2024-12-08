d0dc7e5c8933cc306cd53b625512ddfa7eb6fb7a
Wenyi Kuang <wkuang@nrel.gov>
2024-12-07 22:02:15 -0700

    move p2 with right path

--- postprocessing/transfer_model_files_to_s3_p2.py
+++ postprocessing/transfer_model_files_to_s3_p2.py
@@ -1,7 +1,7 @@
 import comstockpostproc as cspp
 
 #yml_path = '/lustre/eaglefs/projects/enduse/comstock/ymls/euss_fy23/euss_full_350k_short_3of3-116402.yml'
-yml_path = "/kfs2/projects/eusscom/ymls/euss_fy25/euss_2024_r2/full_runs/euss_2024_r2_2of4_37332.yml"
+yml_path = "/kfs2/projects/eusscom/ymls/euss_fy25/euss_2024_r2/full_runs/euss_2024_r2_2of4_37331.yml"
 
 #s3_output_dir = 's3://oedi-data-lake/nrel-pds-building-stock/end-use-load-profiles-for-us-building-stock/2023/comstock_amy2018_release_1/building_energy_models/'
 s3_output_dir = 's3://oedi-data-lake/nrel-pds-building-stock/end-use-load-profiles-for-us-building-stock/2024/comstock_amy2018_release_2/building_energy_models/'
