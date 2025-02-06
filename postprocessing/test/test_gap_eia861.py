d0016fefbe034e311b887f40b5a6b5d5b31abf1c
Eric Ringold <ericringold@gmail.com>
2025-02-06 15:06:21 -0700

    test eia861 class

---
+++ postprocessing/test/test_gap_eia861.py
@@ -0,0 +1,32 @@
+import os
+import pytest
+import logging
+
+import comstockpostproc.gap.eia861
+
+def test_eia_861():
+    # annual, all data
+    annual_all = comstockpostproc.gap.eia861.EIA861(freq='Annual')
+
+    print(annual_all.data.columns)
+    checks = ['COMMERCIAL', 'RESIDENTIAL', 'INDUSTRIAL', 'TOTAL', 'Sales', 'Customers', 'Revenues']
+    assert(all(any(item in col for col in annual_all.data.columns) for item in checks))
+
+    assert(os.path.exists(os.path.join(annual_all.truth_data_dir, 'Sales_Ult_Cust_2018.xlsx')))
+    assert(os.path.exists(os.path.join(annual_all.processed_dir, 'eia861_Annual_2018_All_All.csv')))
+
+    # annual, commercial-only
+    annual_com = comstockpostproc.gap.eia861.EIA861(freq='Annual', segment='Commercial')
+    missing = ['RESIDENTIAL', 'INDUSTRIAL', 'TOTAL', 'Sales', 'Customers', 'Revenues']
+    assert(all(any(item not in col for col in annual_com.data.columns) for item in missing))
+    present = ['COMMERCIAL', 'Sales', 'Customers', 'Revenues']
+    assert(all(any(item in col for col in annual_com.data.columns) for item in present))
+    assert(os.path.exists(os.path.join(annual_com.processed_dir, 'eia861_Annual_2018_Commercial_All.csv')))
+
+    # annual, commercial sales
+    annual_com_sales = comstockpostproc.gap.eia861.EIA861(freq='Annual', segment='Commercial', measure='Sales')
+    missing = ['RESIDENTIAL', 'INDUSTRIAL', 'TOTAL', 'Customers', 'Revenues']
+    assert(all(any(segment not in col for col in annual_com_sales.data.columns) for segment in missing))
+    present = ['COMMERCIAL', 'Sales']
+    assert(all(any(item in col for col in annual_com_sales.data.columns) for item in present))
+    assert(os.path.exists(os.path.join(annual_com_sales.processed_dir, 'eia861_Annual_2018_Commercial_Sales.csv')))
