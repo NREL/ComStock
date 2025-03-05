2f65da9bcacf788ed717fa4a2bee6e7a36303f93
Janghyun(JK)Kim <janghyun.kim@nrel.gov>
2025-03-05 13:05:14 -0700

    initialize iplv value if not available

--- measures/comstock_sensitivity_reports/measure.rb
+++ measures/comstock_sensitivity_reports/measure.rb
@@ -2054,6 +2054,7 @@ class ComStockSensitivityReports < OpenStudio::Measure::ReportingMeasure
       if val.is_initialized
         chiller_iplv_eer = val.get
       else
+        chiller_iplv_eer = 0.0
         runner.registerWarning('Chiller IPLV EER not available.')
       end
 
