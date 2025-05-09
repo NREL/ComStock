52f471520da3e7de3c1c6c9cca17c6706fd81eb6
Landan Taylor <landan.taylor@nrel.gov>
2025-05-09 09:26:09 -0600

    Adding Grocery Stores to Naming Mixin

--- postprocessing/comstockpostproc/naming_mixin.py
+++ postprocessing/comstockpostproc/naming_mixin.py
@@ -704,7 +704,8 @@ class NamingMixin():
             'Hospital',
             'SmallHotel',
             'LargeHotel',
-            'Warehouse'],
+            'Warehouse',
+            'Grocery'],
 
          HVAC_SYS: [
              'Baseboard electric',
@@ -766,7 +767,8 @@ class NamingMixin():
         'Hospital': 'hospital',
         'SmallHotel': 'small_hotel',
         'LargeHotel': 'large_hotel',
-        'Warehouse': 'warehouse'
+        'Warehouse': 'warehouse',
+        'Grocery': 'grocery'
     }
 
     BLDG_TYPE_TO_ABBRV = {
@@ -783,7 +785,8 @@ class NamingMixin():
         'Hospital': 'HSP',
         'SmallHotel': 'SMHOT',
         'LargeHotel': 'LGHOT',
-        'Warehouse': 'WH'
+        'Warehouse': 'WH',
+        'Grocery': 'GROC'
     }
 
     END_USES = [
