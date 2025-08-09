33de3c6ca16910d934076528db54738d60af4f28
Allen <aallen@nrel.gov>
2025-08-09 17:49:47 -0600

    measure test

--- resources/measures/upgrade_hvac_fan_static_pressure_reset/tests/fan_static_pressure_reset_test.rb
+++ resources/measures/upgrade_hvac_fan_static_pressure_reset/tests/fan_static_pressure_reset_test.rb
@@ -8,130 +8,231 @@ require 'fileutils'
 require_relative '../measure'
 
 class FanStaticPressureResetTest < Minitest::Test
-  # def setup
-  # end
-
-  # def teardown
-  # end
-
-  def test_number_of_arguments_and_argument_names
-    # create an instance of the measure
-    measure = FanStaticPressureReset.new
-
-    # make an empty model
-    model = OpenStudio::Model::Model.new
-
-    # get arguments and test that they are what we are expecting
-    arguments = measure.arguments(model)
-    assert_equal(1, arguments.size)
-    assert_equal('space_name', arguments[0].name)
+  # return file paths to test models in test directory
+  def models_for_tests
+    paths = Dir.glob(File.join(File.dirname(__FILE__), '../../../tests/models/*.osm'))
+    paths.map { |path| File.expand_path(path) }
   end
 
-  def test_bad_argument_values
-    # create an instance of the measure
-    measure = FanStaticPressureReset.new
-
-    # create runner with empty OSW
-    osw = OpenStudio::WorkflowJSON.new
-    runner = OpenStudio::Measure::OSRunner.new(osw)
-
-    # make an empty model
-    model = OpenStudio::Model::Model.new
-
-    # get arguments
-    arguments = measure.arguments(model)
-    argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
-
-    # create hash of argument values
-    args_hash = {}
-    args_hash['space_name'] = ''
-
-    # populate argument with specified hash value if specified
-    arguments.each do |arg|
-      temp_arg_var = arg.clone
-      if args_hash.key?(arg.name)
-        assert(temp_arg_var.setValue(args_hash[arg.name]))
-      end
-      argument_map[arg.name] = temp_arg_var
-    end
-
-    # run the measure
-    measure.run(model, runner, argument_map)
-    result = runner.result
-
-    # show the output
-    show_output(result)
-
-    # assert that it did fail
-    assert_equal('Fail', result.value.valueName)
-    assert_equal(1, result.stepErrors.size)
-    assert_match(/empty space name/i, result.stepErrors.first)
+  # return file paths to epw files in test directory
+  def epws_for_tests
+    paths = Dir.glob(File.join(File.dirname(__FILE__), '../../../tests/weather/*.epw'))
+    paths.map { |path| File.expand_path(path) }
   end
 
-  def test_good_argument_values
-    # create an instance of the measure
-    measure = FanStaticPressureReset.new
+  def load_model(osm_path)
+    translator = OpenStudio::OSVersion::VersionTranslator.new
+    model = translator.loadModel(OpenStudio::Path.new(osm_path))
+    assert(!model.empty?)
+    model.get
+  end
 
-    # create runner with empty OSW
-    osw = OpenStudio::WorkflowJSON.new
-    runner = OpenStudio::Measure::OSRunner.new(osw)
+  def run_dir(test_name)
+    # always generate test output in specially named 'output' directory so result files are not made part of the measure
+    "#{File.dirname(__FILE__)}/output/#{test_name}"
+  end
+
+  def model_input_path(osm_name)
+    # return models_for_tests.select { |x| set[:model] == osm_name }
+    File.join(File.dirname(__FILE__), '../../../tests/models', osm_name)
+  end
+
+  def epw_input_path(epw_name)
+    File.join(File.dirname(__FILE__), '../../../tests/weather', epw_name)
+  end
+
+  def model_output_path(test_name)
+    "#{run_dir(test_name)}/#{test_name}.osm"
+  end
+
+  def sql_path(test_name)
+    "#{run_dir(test_name)}/run/eplusout.sql"
+  end
+
+  def report_path(test_name)
+    "#{run_dir(test_name)}/reports/eplustbl.html"
+  end
+   # applies the measure and then runs the model
+  def set_weather_and_apply_measure_and_run(test_name, measure, argument_map, osm_path, epw_path, run_model: false, model: nil, apply: true, expected_results: 'Success')
+    assert(File.exist?(osm_path))
+    assert(File.exist?(epw_path))
+    ddy_path = "#{epw_path.gsub('.epw', '')}.ddy"
+
+    # create run directory if it does not exist
+    FileUtils.mkdir_p(run_dir(test_name)) unless File.exist?(run_dir(test_name))
+    assert(File.exist?(run_dir(test_name)))
+
+    # change into run directory for tests
+    start_dir = Dir.pwd
+    Dir.chdir run_dir(test_name)
+
+    # remove prior runs if they exist
+    FileUtils.rm(model_output_path(test_name)) if File.exist?(model_output_path(test_name))
+    FileUtils.rm(report_path(test_name)) if File.exist?(report_path(test_name))
+
+    # copy the osm and epw to the test directory
+    new_osm_path = "#{run_dir(test_name)}/#{File.basename(osm_path)}"
+    FileUtils.cp(osm_path, new_osm_path)
+    new_epw_path = "#{run_dir(test_name)}/#{File.basename(epw_path)}"
+    FileUtils.cp(epw_path, new_epw_path)
+    # create an instance of a runner
+    runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
 
     # load the test model
-    translator = OpenStudio::OSVersion::VersionTranslator.new
-    path = "#{File.dirname(__FILE__)}/example_model.osm"
-    model = translator.loadModel(path)
-    refute_empty(model)
-    model = model.get
+    model = load_model(new_osm_path) if model.nil?
 
-    # store the number of spaces in the seed model
-    num_spaces_seed = model.getSpaces.size
+    # set model weather file
+    epw_file = OpenStudio::EpwFile.new(OpenStudio::Path.new(new_epw_path))
+    OpenStudio::Model::WeatherFile.setWeatherFile(model, epw_file)
+    assert(model.weatherFile.is_initialized)
 
-    # get arguments
-    arguments = measure.arguments(model)
-    argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
+    # set design days
+    if File.exist?(ddy_path)
 
-    # create hash of argument values.
-    # If the argument has a default that you want to use, you don't need it in the hash
-    args_hash = {}
-    args_hash['space_name'] = 'New Space'
-    # using defaults values from measure.rb for other arguments
+      # remove all the Design Day objects that are in the file
+      model.getObjectsByType('OS:SizingPeriod:DesignDay'.to_IddObjectType).each(&:remove)
 
-    # populate argument with specified hash value if specified
-    arguments.each do |arg|
-      temp_arg_var = arg.clone
-      if args_hash.key?(arg.name)
-        assert(temp_arg_var.setValue(args_hash[arg.name]))
+      # load ddy
+      ddy_model = OpenStudio::EnergyPlus.loadAndTranslateIdf(ddy_path).get
+
+      ddy_model.getDesignDays.sort.each do |d|
+        # grab only the ones that matter
+        ddy_list = [
+          /Htg 99.6. Condns DB/, # Annual heating 99.6%
+          /Clg .4. Condns WB=>MDB/, # Annual humidity (for cooling towers and evap coolers)
+          /Clg .4. Condns DB=>MWB/, # Annual cooling
+          /August .4. Condns DB=>MCWB/, # Monthly cooling DB=>MCWB (to handle solar-gain-driven cooling)
+          /September .4. Condns DB=>MCWB/,
+          /October .4. Condns DB=>MCWB/
+        ]
+        ddy_list.each do |ddy_name_regex|
+          if d.name.get.to_s.match?(ddy_name_regex)
+            runner.registerInfo("Adding object #{d.name}")
+
+            # add the object to the existing model
+            model.addObject(d.clone)
+            break
+          end
+        end
       end
-      argument_map[arg.name] = temp_arg_var
+
+      # assert
+      assert_equal(false, model.getDesignDays.size.zero?)
     end
 
-    # Ensure the model did not start with a space named like requested
-    refute_includes(model.getSpaces.map(&:nameString), "New Space")
+    if apply
+      # run the measure
+      puts "\nAPPLYING MEASURE..."
+      measure.run(model, runner, argument_map)
+      result = runner.result
+      result_success = result.value.valueName == 'Success'
+      assert_equal(expected_results, result.value.valueName)
 
-    # run the measure
-    measure.run(model, runner, argument_map)
-    result = runner.result
+      # Show the output
+      show_output(result)
+    end
 
-    # show the output
-    show_output(result)
+    # adding output variables (for debugging)
+    out_vars = [
+      'Air System Mixed Air Mass Flow Rate',
+      'Fan Air Mass Flow Rate',
+      'Unitary System Predicted Sensible Load to Setpoint Heat Transfer Rate',
+      'Cooling Coil Total Cooling Rate',
+      'Cooling Coil Electricity Rate',
+      'Cooling Coil Runtime Fraction',
+      'Heating Coil Heating Rate',
+      'Heating Coil Electricity Rate',
+      'Heating Coil Runtime Fraction',
+      'Unitary System DX Coil Cycling Ratio',
+      'Unitary System DX Coil Speed Ratio',
+      'Unitary System DX Coil Speed Level',
+      'Unitary System Total Cooling Rate',
+      'Unitary System Total Heating Rate',
+      'Unitary System Electricity Rate',
+      'HVAC System Solver Iteration Count',
+      'Site Outdoor Air Drybulb Temperature',
+      'Heating Coil Crankcase Heater Electricity Rate',
+      'Heating Coil Defrost Electricity Rate',
+      'Zone Windows Total Transmitted Solar Radiation Rate',
+    ]
+    out_vars.each do |out_var_name|
+        ov = OpenStudio::Model::OutputVariable.new('ov', model)
+        ov.setKeyValue('*')
+        ov.setReportingFrequency('hourly')
+        ov.setVariableName(out_var_name)
+    end
+    model.getOutputControlFiles.setOutputCSV(true)
 
-    # assert that it ran correctly
-    assert_equal('Success', result.value.valueName)
-    assert_equal(1, result.stepInfo.size)
-    assert_empty(result.stepWarnings)
+    # Save model
+    model.save(model_output_path(test_name), true)
 
-    # check that there is now 1 extra space
-    assert_equal(1, model.getSpaces.size - num_spaces_seed)
-    assert_includes(model.getSpaces.map(&:nameString), "New Space")
+    if run_model
+      puts "\nRUNNING MODEL..."
 
-    refute_empty(result.stepInitialCondition)
-    assert_equal('The building started with 4 spaces.', result.stepInitialCondition.get)
+      std = Standard.build('90.1-2013')
+      std.model_run_simulation_and_log_errors(model, run_dir(test_name))
 
-    refute_empty(result.stepFinalCondition)
-    assert_equal('The building finished with 5 spaces.', result.stepFinalCondition.get)
+      # Check that the model ran successfully
+      assert(File.exist?(sql_path(test_name)))
+    end
 
-    # save the model to test output directory
-    output_file_path = "#{File.dirname(__FILE__)}/output/test_output.osm"
-    model.save(output_file_path, true)
+    # change back directory
+    Dir.chdir(start_dir)
+
+    result
   end
-end
+
+  def test_confirm_fan_curve_change
+  
+    osm_name = 'SP_reset_measure_Test.osm'
+    epw_name = 'USA_AL_Maxwell.AFB.722265_TMY3.epw'
+
+    test_name = 'confirm_fan_curve_change'
+
+    puts "\n######\nTEST:#{test_name}\n######\n"
+
+    osm_path = model_input_path(osm_name)
+	epw_path = epw_input_path(epw_name)
+
+    # Create an instance of the measure
+    measure = FanStaticPressureReset.new
+
+    # Load the model
+    model = load_model(osm_path)
+	
+	# get arguments
+    arguments = measure.arguments(model)
+    argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
+	
+    # run the measure
+    result = set_weather_and_apply_measure_and_run(__method__, measure, argument_map, osm_path, epw_path,
+                                                   run_model: false)
+    assert_equal('Success', result.value.valueName)
+    model = load_model(model_output_path(__method__))
+	
+	#Coefficients for fan curve emulating SP reset 
+	reset_coeff_1 = 0.040759894
+    reset_coeff_2 = 0.08804497
+    reset_coeff_3 = -0.07292612
+    reset_coeff_4 = 0.943739823
+	
+	reset_arr = [reset_coeff_1, reset_coeff_2, reset_coeff_3, reset_coeff_4]
+
+	vs_fans = model.getFanVariableVolumes
+	
+	for fan in vs_fans
+	    fan = fan.to_FanVariableVolume.get
+		coeff_1 = fan.fanPowerCoefficient1()
+		coeff_2 = fan.fanPowerCoefficient2()
+		coeff_3 = fan.fanPowerCoefficient3()
+		coeff_4 = fan.fanPowerCoefficient4()
+		fan_coeff_arr = [coeff_1, coeff_2, coeff_3, coeff_4]
+		if fan_coeff_arr != reset_arr
+		   return false
+		end 
+	end 
+	
+	return true 
+	end 
+  
+  end 
