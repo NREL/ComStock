b8e536287ac63380c50c1507ed1a436811313b37
Allen <aallen@nrel.gov>
2025-08-10 13:41:30 -0600

    measure test

--- resources/measures/upgrade_add_thermostat_setback/tests/upgrade_add_thermostat_setback_test.rb
+++ resources/measures/upgrade_add_thermostat_setback/tests/upgrade_add_thermostat_setback_test.rb
@@ -8,130 +8,260 @@ require 'fileutils'
 require_relative '../measure'
 
 class UpgradeAddThermostatSetbackTest < Minitest::Test
-  # def setup
-  # end
-
-  # def teardown
-  # end
-
-  def test_number_of_arguments_and_argument_names
-    # create an instance of the measure
-    measure = UpgradeAddThermostatSetback.new
-
-    # make an empty model
-    model = OpenStudio::Model::Model.new
-
-    # get arguments and test that they are what we are expecting
-    arguments = measure.arguments(model)
-    assert_equal(1, arguments.size)
-    assert_equal('space_name', arguments[0].name)
+  def models_for_tests
+    paths = Dir.glob(File.join(File.dirname(__FILE__), '../../../tests/models/*.osm'))
+    paths.map { |path| File.expand_path(path) }
   end
 
-  def test_bad_argument_values
-    # create an instance of the measure
-    measure = UpgradeAddThermostatSetback.new
-
-    # create runner with empty OSW
-    osw = OpenStudio::WorkflowJSON.new
-    runner = OpenStudio::Measure::OSRunner.new(osw)
-
-    # make an empty model
-    model = OpenStudio::Model::Model.new
-
-    # get arguments
-    arguments = measure.arguments(model)
-    argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
-
-    # create hash of argument values
-    args_hash = {}
-    args_hash['space_name'] = ''
-
-    # populate argument with specified hash value if specified
-    arguments.each do |arg|
-      temp_arg_var = arg.clone
-      if args_hash.key?(arg.name)
-        assert(temp_arg_var.setValue(args_hash[arg.name]))
-      end
-      argument_map[arg.name] = temp_arg_var
-    end
-
-    # run the measure
-    measure.run(model, runner, argument_map)
-    result = runner.result
-
-    # show the output
-    show_output(result)
-
-    # assert that it did fail
-    assert_equal('Fail', result.value.valueName)
-    assert_equal(1, result.stepErrors.size)
-    assert_match(/empty space name/i, result.stepErrors.first)
+  # return file paths to epw files in test directory
+  def epws_for_tests
+    paths = Dir.glob(File.join(File.dirname(__FILE__), '../../../tests/weather/*.epw'))
+    paths.map { |path| File.expand_path(path) }
   end
 
-  def test_good_argument_values
-    # create an instance of the measure
-    measure = UpgradeAddThermostatSetback.new
+  def load_model(osm_path)
+    translator = OpenStudio::OSVersion::VersionTranslator.new
+    model = translator.loadModel(OpenStudio::Path.new(osm_path))
+    assert(!model.empty?)
+    model.get
+  end
 
-    # create runner with empty OSW
-    osw = OpenStudio::WorkflowJSON.new
-    runner = OpenStudio::Measure::OSRunner.new(osw)
+  def run_dir(test_name)
+    # always generate test output in specially named 'output' directory so result files are not made part of the measure
+    "#{File.dirname(__FILE__)}/output/#{test_name}"
+  end
+
+  def model_input_path(osm_name)
+    # return models_for_tests.select { |x| set[:model] == osm_name }
+    File.join(File.dirname(__FILE__), '../../../tests/models', osm_name)
+  end
+
+  def epw_input_path(epw_name)
+    File.join(File.dirname(__FILE__), '../../../tests/weather', epw_name)
+  end
+
+  def model_output_path(test_name)
+    "#{run_dir(test_name)}/#{test_name}.osm"
+  end
+
+  def sql_path(test_name)
+    "#{run_dir(test_name)}/run/eplusout.sql"
+  end
+
+  def report_path(test_name)
+    "#{run_dir(test_name)}/reports/eplustbl.html"
+  end
+
+  # applies the measure and then runs the model
+  def set_weather_and_apply_measure_and_run(test_name, measure, argument_map, osm_path, epw_path, run_model: false, model: nil, apply: true, expected_results: 'Success')
+    assert(File.exist?(osm_path))
+    assert(File.exist?(epw_path))
+    ddy_path = "#{epw_path.gsub('.epw', '')}.ddy"
+
+    # create run directory if it does not exist
+    FileUtils.mkdir_p(run_dir(test_name)) unless File.exist?(run_dir(test_name))
+    assert(File.exist?(run_dir(test_name)))
+
+    # change into run directory for tests
+    start_dir = Dir.pwd
+    Dir.chdir run_dir(test_name)
+
+    # remove prior runs if they exist
+    FileUtils.rm(model_output_path(test_name)) if File.exist?(model_output_path(test_name))
+    FileUtils.rm(report_path(test_name)) if File.exist?(report_path(test_name))
+
+    # copy the osm and epw to the test directory
+    new_osm_path = "#{run_dir(test_name)}/#{File.basename(osm_path)}"
+    FileUtils.cp(osm_path, new_osm_path)
+    new_epw_path = "#{run_dir(test_name)}/#{File.basename(epw_path)}"
+    FileUtils.cp(epw_path, new_epw_path)
+    # create an instance of a runner
+    runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
 
     # load the test model
-    translator = OpenStudio::OSVersion::VersionTranslator.new
-    path = "#{File.dirname(__FILE__)}/example_model.osm"
-    model = translator.loadModel(path)
-    refute_empty(model)
-    model = model.get
+    model = load_model(new_osm_path) if model.nil?
 
-    # store the number of spaces in the seed model
-    num_spaces_seed = model.getSpaces.size
+    # set model weather file
+    epw_file = OpenStudio::EpwFile.new(OpenStudio::Path.new(new_epw_path))
+    OpenStudio::Model::WeatherFile.setWeatherFile(model, epw_file)
+    assert(model.weatherFile.is_initialized)
+
+    # set design days
+    if File.exist?(ddy_path)
+
+      # remove all the Design Day objects that are in the file
+      model.getObjectsByType('OS:SizingPeriod:DesignDay'.to_IddObjectType).each(&:remove)
+
+      # load ddy
+      ddy_model = OpenStudio::EnergyPlus.loadAndTranslateIdf(ddy_path).get
+
+      ddy_model.getDesignDays.sort.each do |d|
+        # grab only the ones that matter
+        ddy_list = [
+          /Htg 99.6. Condns DB/, # Annual heating 99.6%
+          /Clg .4. Condns WB=>MDB/, # Annual humidity (for cooling towers and evap coolers)
+          /Clg .4. Condns DB=>MWB/, # Annual cooling
+          /August .4. Condns DB=>MCWB/, # Monthly cooling DB=>MCWB (to handle solar-gain-driven cooling)
+          /September .4. Condns DB=>MCWB/,
+          /October .4. Condns DB=>MCWB/
+        ]
+        ddy_list.each do |ddy_name_regex|
+          if d.name.get.to_s.match?(ddy_name_regex)
+            runner.registerInfo("Adding object #{d.name}")
+
+            # add the object to the existing model
+            model.addObject(d.clone)
+            break
+          end
+        end
+      end
+
+      # assert
+      assert_equal(false, model.getDesignDays.size.zero?)
+    end
+
+    if apply
+      # run the measure
+      puts "\nAPPLYING MEASURE..."
+      measure.run(model, runner, argument_map)
+      result = runner.result
+      result_success = result.value.valueName == 'Success'
+      assert_equal(expected_results, result.value.valueName)
+
+      # Show the output
+      show_output(result)
+    end
+
+    model.getOutputControlFiles.setOutputCSV(true)
+
+    # Save model
+    model.save(model_output_path(test_name), true)
+
+    if run_model
+      puts "\nRUNNING MODEL..."
+
+      std = Standard.build('90.1-2013')
+      std.model_run_simulation_and_log_errors(model, run_dir(test_name))
+
+      # Check that the model ran successfully
+      assert(File.exist?(sql_path(test_name)))
+    end
+
+    # change back directory
+    Dir.chdir(start_dir)
+
+    result
+  end
+  def test_number_of_arguments_and_argument_names
+    # This test ensures that the current test is matched to the measure inputs
+    test_name = 'test_number_of_arguments_and_argument_names'
+    puts "\n######\nTEST:#{test_name}\n######\n"
+
+    # Create an instance of the measure
+    measure = UpgradeAddThermostatSetback.new
+
+    # Make an empty model
+    model = OpenStudio::Model::Model.new
+
+    # Get arguments and test that they are what we are expecting
+    arguments = measure.arguments(model)
+    assert_equal(6, arguments.size)
+    assert_equal('clg_setback', arguments[0].name)
+    assert_equal('htg_setback', arguments[1].name)
+    assert_equal('opt_start', arguments[2].name)
+    assert_equal('opt_start_len', arguments[3].name)
+    assert_equal('htg_min', arguments[4].name)
+    assert_equal('clg_max', arguments[5].name)
+  end
+  def test_confirm_heating_setback_change_square_wave
+    # confirm that any heating setbacks are now 2F
+    osm_name = '380_psz_ac_with_gas_boiler._setback_test.osm'
+    epw_name = 'NE_Kearney_Muni_725526_16.epw'
+
+    test_name = 'confirm_heating_setback_change_square_wave'
+
+    puts "\n######\nTEST:#{test_name}\n######\n"
+
+    osm_path = model_input_path(osm_name)
+    epw_input_path(epw_name)
+
+    # Create an instance of the measure
+    measure = UpgradeAddThermostatSetback.new
+
+    # Load the model
+    model = load_model(osm_path)
 
     # get arguments
     arguments = measure.arguments(model)
     argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
 
-    # create hash of argument values.
-    # If the argument has a default that you want to use, you don't need it in the hash
-    args_hash = {}
-    args_hash['space_name'] = 'New Space'
-    # using defaults values from measure.rb for other arguments
-
-    # populate argument with specified hash value if specified
-    arguments.each do |arg|
-      temp_arg_var = arg.clone
-      if args_hash.key?(arg.name)
-        assert(temp_arg_var.setValue(args_hash[arg.name]))
-      end
-      argument_map[arg.name] = temp_arg_var
-    end
-
-    # Ensure the model did not start with a space named like requested
-    refute_includes(model.getSpaces.map(&:nameString), "New Space")
+    osm_path = model_input_path(osm_name)
+    epw_path = epw_input_path(epw_name)
+    
+	conv_factor = fraction_as_rational = Rational(5, 9) 
+    htg_setback = 5.0
+    htg_setback_c = htg_setback.to_f * conv_factor
+	
+	#Use default argument values 
 
     # run the measure
-    measure.run(model, runner, argument_map)
-    result = runner.result
-
-    # show the output
-    show_output(result)
-
-    # assert that it ran correctly
+    result = set_weather_and_apply_measure_and_run(__method__, measure, argument_map, osm_path, epw_path,
+                                                   run_model: false)
     assert_equal('Success', result.value.valueName)
-    assert_equal(1, result.stepInfo.size)
-    assert_empty(result.stepWarnings)
+    model = load_model(model_output_path(__method__))
 
-    # check that there is now 1 extra space
-    assert_equal(1, model.getSpaces.size - num_spaces_seed)
-    assert_includes(model.getSpaces.map(&:nameString), "New Space")
+    schedule_deltas = [] # keep track of differences between min and max values in schedules
+	
+	schedule_deltas = [] # keep track of differences between min and max values in schedules
 
-    refute_empty(result.stepInitialCondition)
-    assert_equal('The building started with 4 spaces.', result.stepInitialCondition.get)
 
-    refute_empty(result.stepFinalCondition)
-    assert_equal('The building finished with 5 spaces.', result.stepFinalCondition.get)
+    # Loop thru zones and look at temp setbacks
+    model.getAirLoopHVACs.sort.each do |air_loop_hvac|
+      zones = air_loop_hvac.thermalZones
 
-    # save the model to test output directory
-    output_file_path = "#{File.dirname(__FILE__)}/output/test_output.osm"
-    model.save(output_file_path, true)
+
+      zones.sort.each do |thermal_zone|
+        next unless thermal_zone.thermostatSetpointDualSetpoint.is_initialized
+
+        zone_thermostat = thermal_zone.thermostatSetpointDualSetpoint.get
+        htg_schedule = zone_thermostat.heatingSetpointTemperatureSchedule
+        if htg_schedule.empty?
+          puts("Heating setpoint schedule not found for zone '#{zone.name.get}'")
+          next
+        elsif htg_schedule.get.to_ScheduleRuleset.empty?
+          puts("Schedule '#{htg_schedule.get.name.get}' is not a ScheduleRuleset, will not be adjusted")
+          next
+        else
+          htg_schedule = htg_schedule.get.to_ScheduleRuleset.get
+        end
+        profiles = [htg_schedule.defaultDaySchedule]
+        htg_schedule.scheduleRules.each { |rule| profiles << rule.daySchedule }
+        profiles.sort.each do |tstat_profile|
+          #working_profile = tstat_profile.values.dup
+          tstat_profile_min = tstat_profile.values.min
+          tstat_profile_max = tstat_profile.values.max
+          # tstat_profile.values.each_with_index do |value, i| # find minimum except for values during opt start
+            # # test values for optimum start (need to be at least the third timestep in the profile to test)
+            # if i > 3 && possible_opt_start(i, tstat_profile, tstat_profile_min) # identify if the time step could have been part of an optimum start
+              # working_profile.delete(value)
+            # end
+          # end
+          # tstat_profile_min_adj = working_profile.min
+          # tstat_profile_max_adj = working_profile.max
+          schedule_deltas << tstat_profile_max - tstat_profile_min # assuming that any changes in the schedule during the day represent nighttime setbacks
+        end
+      end
+    end
+
+    # Make sure no deltas are greater than the expected setback value
+    deltas_out_of_range = schedule_deltas.any? { |x| x > htg_setback_c + 0.2 } #add a margin for unit conversion 
+	
+	
+	puts("Temperature deltas in schedule match expected values: #{(deltas_out_of_range == false)}")
+	puts schedule_deltas
+	puts htg_setback_c 
+
+    assert_equal(deltas_out_of_range, false)
+    true
   end
 end
