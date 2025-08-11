a70243edcbf2c8604e8a1b59c98adcdd55ac8f2d
JanghyunJK <janghyun.kim@nrel.gov>
2025-08-11 15:08:42 -0600

    rubocop

--- resources/measures/upgrade_hvac_pump/tests/upgrade_hvac_pump_test.rb
+++ resources/measures/upgrade_hvac_pump/tests/upgrade_hvac_pump_test.rb
@@ -94,7 +94,8 @@ class UpgradeHvacPumpTest < Minitest::Test
 
   # supporting method: return run path
   def run_dir(test_name)
-    # always generate test output in specially named 'output' directory so result files are not made part of the measure
+    # always generate test output in specially named 'output' directory so
+    # result files are not made part of the measure
     "#{File.dirname(__FILE__)}/output/#{test_name}"
   end
 
@@ -170,7 +171,8 @@ class UpgradeHvacPumpTest < Minitest::Test
     pump_var_part_load_curve_coeff4_weighted_avg_before = pump_specs_var_spd_before[7]
 
     # get control specs for baseline
-    fraction_chw_oat_reset_enabled_b, fraction_cw_oat_reset_enabled_b = UpgradeHvacPump.control_specifications(model)
+    fraction_chw_oat_reset_enabled_b, fraction_cw_oat_reset_enabled_b =
+      UpgradeHvacPump.control_specifications(model)
 
     # run the measure
     measure.run(model, runner, argument_map)
@@ -193,7 +195,8 @@ class UpgradeHvacPumpTest < Minitest::Test
     pump_var_part_load_curve_coeff4_weighted_avg_after = pump_specs_var_spd_after[7]
 
     # get control specs for upgrade
-    fraction_chw_oat_reset_enabled_a, fraction_cw_oat_reset_enabled_a = UpgradeHvacPump.control_specifications(model)
+    fraction_chw_oat_reset_enabled_a, fraction_cw_oat_reset_enabled_a =
+      UpgradeHvacPump.control_specifications(model)
 
     # show the output
     show_output(result)
@@ -206,42 +209,93 @@ class UpgradeHvacPumpTest < Minitest::Test
     refute_empty(result.stepFinalCondition)
 
     # check chilled water system specs
-    puts("### DEBUGGING: pump_motor_eff_weighted_average_c_before = #{pump_motor_eff_weighted_average_c_before} | pump_motor_eff_weighted_average_c_after = #{pump_motor_eff_weighted_average_c_after}")
-    puts("### DEBUGGING: pump_motor_bhp_weighted_average_c_before = #{pump_motor_bhp_weighted_average_c_before} | pump_motor_bhp_weighted_average_c_after = #{pump_motor_bhp_weighted_average_c_after}")
-    puts("### DEBUGGING: pump_motor_eff_weighted_average_v_before = #{pump_motor_eff_weighted_average_v_before} | pump_motor_eff_weighted_average_v_after = #{pump_motor_eff_weighted_average_v_after}")
-    puts("### DEBUGGING: pump_motor_bhp_weighted_average_v_before = #{pump_motor_bhp_weighted_average_v_before} | pump_motor_bhp_weighted_average_v_after = #{pump_motor_bhp_weighted_average_v_after}")
-    puts("### DEBUGGING: pump part load curve coeffi 1 = #{pump_var_part_load_curve_coeff1_weighted_avg_before} | coeff1_a = #{pump_var_part_load_curve_coeff1_weighted_avg_after}")
-    puts("### DEBUGGING: pump part load curve coeffi 2 = #{pump_var_part_load_curve_coeff2_weighted_avg_before} | coeff2_a = #{pump_var_part_load_curve_coeff2_weighted_avg_after}")
-    puts("### DEBUGGING: pump part load curve coeffi 3 = #{pump_var_part_load_curve_coeff3_weighted_avg_before} | coeff3_a = #{pump_var_part_load_curve_coeff3_weighted_avg_after}")
-    puts("### DEBUGGING: pump part load curve coeffi 4 = #{pump_var_part_load_curve_coeff4_weighted_avg_before} | coeff4_a = #{pump_var_part_load_curve_coeff4_weighted_avg_after}")
+    puts("### DEBUGGING: pump_motor_eff_weighted_average_c_before = #{
+      pump_motor_eff_weighted_average_c_before
+    } | pump_motor_eff_weighted_average_c_after = #{
+        pump_motor_eff_weighted_average_c_after
+      }")
+    puts("### DEBUGGING: pump_motor_bhp_weighted_average_c_before = #{
+      pump_motor_bhp_weighted_average_c_before
+    } | pump_motor_bhp_weighted_average_c_after = #{
+        pump_motor_bhp_weighted_average_c_after
+      }")
+    puts("### DEBUGGING: pump_motor_eff_weighted_average_v_before = #{
+      pump_motor_eff_weighted_average_v_before
+    } | pump_motor_eff_weighted_average_v_after = #{
+        pump_motor_eff_weighted_average_v_after
+      }")
+    puts("### DEBUGGING: pump_motor_bhp_weighted_average_v_before = #{
+      pump_motor_bhp_weighted_average_v_before
+    } | pump_motor_bhp_weighted_average_v_after = #{
+        pump_motor_bhp_weighted_average_v_after
+      }")
+    puts("### DEBUGGING: pump part load curve coeffi 1 = #{
+      pump_var_part_load_curve_coeff1_weighted_avg_before
+    } | coeff1_a = #{
+        pump_var_part_load_curve_coeff1_weighted_avg_after
+      }")
+    puts("### DEBUGGING: pump part load curve coeffi 2 = #{
+      pump_var_part_load_curve_coeff2_weighted_avg_before
+    } | coeff2_a = #{pump_var_part_load_curve_coeff2_weighted_avg_after}")
+    puts("### DEBUGGING: pump part load curve coeffi 3 = #{
+      pump_var_part_load_curve_coeff3_weighted_avg_before
+    } | coeff3_a = #{pump_var_part_load_curve_coeff3_weighted_avg_after}")
+    puts("### DEBUGGING: pump part load curve coeffi 4 = #{
+      pump_var_part_load_curve_coeff4_weighted_avg_before
+    } | coeff4_a = #{pump_var_part_load_curve_coeff4_weighted_avg_after}")
     coefficient_set_different = false
-    if (pump_var_part_load_curve_coeff1_weighted_avg_before != pump_var_part_load_curve_coeff1_weighted_avg_after) ||
-       (pump_var_part_load_curve_coeff2_weighted_avg_before != pump_var_part_load_curve_coeff2_weighted_avg_after) ||
-       (pump_var_part_load_curve_coeff3_weighted_avg_before != pump_var_part_load_curve_coeff3_weighted_avg_after) ||
-       (pump_var_part_load_curve_coeff4_weighted_avg_before != pump_var_part_load_curve_coeff4_weighted_avg_after)
+    if (pump_var_part_load_curve_coeff1_weighted_avg_before !=
+        pump_var_part_load_curve_coeff1_weighted_avg_after) ||
+       (pump_var_part_load_curve_coeff2_weighted_avg_before !=
+        pump_var_part_load_curve_coeff2_weighted_avg_after) ||
+       (pump_var_part_load_curve_coeff3_weighted_avg_before !=
+        pump_var_part_load_curve_coeff3_weighted_avg_after) ||
+       (pump_var_part_load_curve_coeff4_weighted_avg_before !=
+        pump_var_part_load_curve_coeff4_weighted_avg_after)
       coefficient_set_different = true
     end
-    assert_equal(true, coefficient_set_different, "Pump part load curve coefficients did not change as expected.")
+    assert_equal(true, coefficient_set_different,
+                 'Pump part load curve coefficients did not change as expected.')
 
     # check performance improvement
     if pump_motor_eff_weighted_average_v_after < pump_motor_eff_weighted_average_c_before
-      assert(false,
-             "Pump motor efficiency got worse compared to existing constant speed pump. Before: #{pump_motor_eff_weighted_average_c_before}, After: #{pump_motor_eff_weighted_average_c_after}")
+      assert(
+        false,
+        "Pump motor efficiency got worse compared to existing constant speed pump. Before: #{
+          pump_motor_eff_weighted_average_c_before
+        }, After: #{
+            pump_motor_eff_weighted_average_c_after
+          }"
+      )
     end
     if pump_motor_eff_weighted_average_v_after < pump_motor_eff_weighted_average_v_before
-      assert(false,
-             "Pump motor efficiency got worse compared to existing variable speed pump. Before: #{pump_motor_eff_weighted_average_v_before}, After: #{pump_motor_eff_weighted_average_v_after}")
+      assert(
+        false,
+        "Pump motor efficiency got worse compared to existing variable speed pump. Before: #{
+          pump_motor_eff_weighted_average_v_before
+        }, After: #{pump_motor_eff_weighted_average_v_after}"
+      )
     end
 
     # check control specs
     puts("### DEBUGGING: instance_test_name = #{instance_test_name}")
-    puts("### DEBUGGING: fraction_chw_oat_reset_enabled_b = #{fraction_chw_oat_reset_enabled_b} | fraction_chw_oat_reset_enabled_a = #{fraction_chw_oat_reset_enabled_a}")
-    puts("### DEBUGGING: fraction_cw_oat_reset_enabled_b = #{fraction_cw_oat_reset_enabled_b} | fraction_cw_oat_reset_enabled_a = #{fraction_cw_oat_reset_enabled_a}")
+    puts("### DEBUGGING: fraction_chw_oat_reset_enabled_b = #{
+      fraction_chw_oat_reset_enabled_b
+    } | fraction_chw_oat_reset_enabled_a = #{
+        fraction_chw_oat_reset_enabled_a
+      }")
+    puts("### DEBUGGING: fraction_cw_oat_reset_enabled_b = #{
+      fraction_cw_oat_reset_enabled_b
+    } | fraction_cw_oat_reset_enabled_a = #{
+        fraction_cw_oat_reset_enabled_a
+      }")
     refute_equal(fraction_chw_oat_reset_enabled_b, fraction_chw_oat_reset_enabled_a)
     if instance_test_name.include?('air_cooled')
-      assert_equal(fraction_cw_oat_reset_enabled_a, 0.0, "Fraction CW OAT Reset Enabled A is not equal to 0.0")
+      assert_equal(fraction_cw_oat_reset_enabled_a, 0.0,
+                   'Fraction CW OAT Reset Enabled A is not equal to 0.0')
     else
-      assert_equal(fraction_cw_oat_reset_enabled_a, 1.0, "Fraction CW OAT Reset Enabled A is not equal to 1.0")
+      assert_equal(fraction_cw_oat_reset_enabled_a, 1.0,
+                   'Fraction CW OAT Reset Enabled A is not equal to 1.0')
     end
 
     # # save the model to test output directory
