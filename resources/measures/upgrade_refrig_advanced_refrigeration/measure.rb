e84e37fff6c81b7b4dd386f097fce721bd9fd0b0
Praprost <mprapros@nrel.gov>
2026-07-01 10:42:00 -0600

    add refrigeration measure

---
+++ resources/measures/upgrade_refrig_advanced_refrigeration/measure.rb
@@ -0,0 +1,151 @@
+# ComStock™, Copyright (c) 2025 Alliance for Sustainable Energy, LLC. All rights reserved.
+# See top level LICENSE.txt file for license terms.
+# *******************************************************************************
+# OpenStudio(R), Copyright (c) 2008-2018, Alliance for Sustainable Energy, LLC.
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# (1) Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# (2) Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# (3) Neither the name of the copyright holder nor the names of any contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission from the respective party.
+#
+# (4) Other than as required in clauses (1) and (2), distributions in any form
+# of modifications or other derivative works may not use the "OpenStudio"
+# trademark, "OS", "os", or any other confusingly similar designation without
+# specific prior written permission from Alliance for Sustainable Energy, LLC.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) AND ANY CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S), ANY CONTRIBUTORS, THE
+# UNITED STATES GOVERNMENT, OR THE UNITED STATES DEPARTMENT OF ENERGY, NOR ANY OF
+# THEIR EMPLOYEES, BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# *******************************************************************************
+
+require 'openstudio-standards'
+
+# start the measure
+class UpgradeRefrigeration < OpenStudio::Measure::ModelMeasure
+  # human readable name
+  def name
+    return 'upgrade_refrigeration'
+  end
+
+  # human readable description
+  def description
+    return 'This measure upgrades the refrigeration template in the model to a user selected template.'
+  end
+
+  # human readable description of modeling approach
+  def modeler_description
+    return 'This measure upgrades the refrigeration template in the model to a user selected template. The original refrigeration system will be removed and a new system with the user-defined template will be installed.'
+  end
+
+  # define the arguments that the user will input
+  def arguments(model)
+    args = OpenStudio::Measure::OSArgumentVector.new
+
+    # Only allow forward-looking refrigeration technology levels for this upgrade.
+    refrigeration_template_choices = OpenStudio::StringVector.new
+    refrigeration_template_choices << 'new'
+    refrigeration_template_choices << 'advanced'
+
+    refrigeration_template = OpenStudio::Measure::OSArgument.makeChoiceArgument('refrigeration_template', refrigeration_template_choices, true)
+    refrigeration_template.setDisplayName('Refrigeration Technology Level')
+    refrigeration_template.setDescription('Technology level for refrigerated cases, walkins, compressors, and systems.')
+    refrigeration_template.setDefaultValue('advanced')
+    args << refrigeration_template
+
+    return args
+  end
+
+  # count all refrigeration objects currently in the model
+  def refrigeration_object_count(model)
+    return model.getRefrigerationCases.size + model.getRefrigerationWalkIns.size
+  end
+
+  # remove existing refrigeration objects
+  def remove_existing_refrigeration(model)
+    removed_count = 0
+
+    model.getRefrigerationCases.each do |ref_case|
+      ref_case.remove
+      removed_count += 1
+    end
+
+    model.getRefrigerationWalkIns.each do |walk_in|
+      walk_in.remove
+      removed_count += 1
+    end
+
+    return removed_count
+  end
+
+  # run create_typical_refrigeration in os-standards with user-specified template
+  def create_refrigeration_system(model, runner, refrigeration_template)
+    runner.registerInfo("Creating replacement refrigeration using template '#{refrigeration_template}'.")
+    result = OpenstudioStandards::Refrigeration.create_typical_refrigeration(
+      model,
+      template: refrigeration_template
+    )
+    runner.registerInfo('Refrigeration creation call finished.')
+    return result != false
+  end
+
+  # define what happens when the measure is run
+  def run(model, runner, user_arguments)
+    super(model, runner, user_arguments)
+
+    runner.registerInfo('Starting refrigeration upgrade measure.')
+
+    # validate arguments
+    if !runner.validateUserArguments(arguments(model), user_arguments)
+      return false
+    end
+
+    refrigeration_template = runner.getStringArgumentValue('refrigeration_template', user_arguments)
+    runner.registerInfo("Selected refrigeration template: #{refrigeration_template}")
+
+    # report baseline refrigeration object count
+    initial_count = refrigeration_object_count(model)
+    runner.registerInitialCondition("Model starts with #{initial_count} refrigeration objects.")
+
+    # preserve the existing refrigerated loads
+    removed_count = 0
+    runner.registerInfo('Leaving existing refrigeration loads in place and updating the refrigeration template in situ.')
+
+    # build replacement refrigeration system from selected template
+    runner.registerInfo('Adding replacement refrigeration objects.')
+    if !create_refrigeration_system(model, runner, refrigeration_template)
+      runner.registerError("Failed to create replacement refrigeration system using template '#{refrigeration_template}'.")
+      return false
+    end
+
+    # confirm refrigeration object count after replacement
+    final_count = refrigeration_object_count(model)
+    runner.registerInfo("Post-upgrade refrigeration object count: #{final_count}")
+    if final_count <= 0
+      runner.registerError("No refrigeration objects were found after attempting to apply template '#{refrigeration_template}'.")
+      return false
+    end
+
+    runner.registerFinalCondition("Refrigeration replacement complete: removed #{removed_count} refrigeration objects and created #{final_count} refrigeration objects using '#{refrigeration_template}' template.")
+    return true
+  end
+end
+
+# register the measure to be used by the application
+UpgradeRefrigeration.new.registerWithApplication
